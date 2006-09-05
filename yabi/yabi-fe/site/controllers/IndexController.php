db5eda2832029fd6d669325f5e548c5c27a0d67a
Ploy Tangtulyangkul <tploy@ccg.murdoch.edu.au>
2006-09-05 06:52:14 +0000

    Zend View working

--- yabi/yabi-fe/site/controllers/IndexController.php
+++ yabi/yabi-fe/site/controllers/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 //$Id$
 
-Zend::loadClass('Zend_Controller_Action');
+//Zend::loadClass('Zend_Controller_Action');
  
 class IndexController extends Zend_Controller_Action {
 	
@@ -9,7 +9,31 @@ class IndexController extends Zend_Controller_Action {
      * indexAction
      */
     public function indexAction() {
-        echo "You have reached the index action edited by Ploy!";	
+    	$view = Zend::registry('view');
+        $view->title = 'Dynamic Title';
+        $view->body = 'This is a dynamic body.';        
+        echo $view->render('example.php');
+        
+        /*
+        $temp_file = 'homepage.htm';
+        
+        $view = Zend::registry('view');
+        
+        if (false === $view->isCached($temp_file))
+        {
+            $vars = array();
+            $vars['title'  ] = 'Page <title>';
+            $vars['text'   ] = 'A text is a text & a text is a text.';
+            $vars['numbers'][0] = 12.9;
+            $vars['numbers'][1] = 29;
+            $vars['numbers'][2] = 78;
+            
+            $vars = $view->escape($vars);
+            
+            $view->assign($vars);
+        }
+        
+        $view->output($temp_file);*/        
     }
 
 
@@ -17,7 +41,7 @@ class IndexController extends Zend_Controller_Action {
      * noRouteAction
      */
     public function noRouteAction() {
-        $this->_redirect('/');
+        $this->_redirect('./');
     }
 }
 ?>
