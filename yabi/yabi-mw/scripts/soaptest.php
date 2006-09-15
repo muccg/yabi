dc1b4637b58b7101392b00f2d0e4b6dc9b194836
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2006-09-15 07:20:01 +0000

    refining deploy, build and compile phases

--- yabi/yabi-mw/scripts/soaptest.php
+++ yabi/yabi-mw/scripts/soaptest.php
@@ -1,5 +1,5 @@
 <?php
-$client = new SoapClient("http://boromir.localdomain:8080/axis/services/yabiws?wsdl");
+$client = new SoapClient("http://boromir.localdomain:8080/axis-ntakayama/services/yabiws?wsdl");
 try {
     $result = $client->isAlive();
 } catch (SoapFault $e) {
