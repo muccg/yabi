1788440a97eb7c15c318721a5b50a131db0cc528
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2006-09-20 09:07:30 +0000

    adding ability to list instances of a process definition in XML

---
+++ yabi/yabi-mw/src/au/edu/murdoch/ccg/yabi/webservice/actions/InstancesList.java
@@ -0,0 +1,75 @@
+package au.edu.murdoch.ccg.yabi.webservice.actions;
+
+import org.jbpm.graph.def.*;
+import org.jbpm.graph.exe.*;
+import org.jbpm.*;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class InstancesList extends Action {
+
+    public InstancesList () {
+        super();
+    }
+
+    public ActionForward execute(
+        ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,
+        HttpServletResponse response) throws Exception {
+
+        JbpmConfiguration jbpmConfiguration = JbpmConfiguration.getInstance("yabi-jbpm.cfg.xml");
+
+        JbpmContext jbpm = jbpmConfiguration.createJbpmContext();
+
+        try {
+            long procDefId = Long.parseLong(request.getParameter("id"));
+
+            ProcessDefinition pd = jbpm.getGraphSession().getProcessDefinition( procDefId );
+
+            request.setAttribute("processDefinition", pd);
+
+            List instances = jbpm.getGraphSession().findProcessInstances( procDefId );
+            HashMap tokens = new HashMap();
+            HashMap contextVariables = new HashMap();
+
+            Iterator iter = instances.iterator();
+            while (iter.hasNext()) {
+                ProcessInstance pi = (ProcessInstance) iter.next();
+
+                List pitokens = pi.findAllTokens();
+
+                //force load of node info
+                Iterator tokenIter = pitokens.iterator();
+                while (tokenIter.hasNext()) {
+                    Token thisToken = (Token) tokenIter.next();
+                    String throwAway = thisToken.getNode().getFullyQualifiedName();
+                }
+
+                tokens.put( new Long(pi.getId()), pitokens );
+
+                Map pivars = pi.getContextInstance().getVariables();
+                contextVariables.put( new Long(pi.getId()), pivars );
+            }
+
+            request.setAttribute("processInstances", instances);
+            request.setAttribute("tokens", tokens);
+            request.setAttribute("contextVariables", contextVariables);
+
+        } catch (Exception e) {
+        } finally {
+            jbpm.close();
+        }
+
+        return mapping.findForward("success");
+
+    }
+
+}
