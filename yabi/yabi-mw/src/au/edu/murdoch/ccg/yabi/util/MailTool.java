07b4eace48019fe1e74084e746f84f6dbf71d57a
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2007-10-30 07:20:18 +0000

    adding emailing support

---
+++ yabi/yabi-mw/src/au/edu/murdoch/ccg/yabi/util/MailTool.java
@@ -0,0 +1,89 @@
+//
+//  MailTool.java
+//  yabi-mw
+//
+//  Created by ntt on 30/10/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+package au.edu.murdoch.ccg.yabi.util;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+import au.edu.murdoch.ccg.yabi.util.YabiConfiguration;
+import au.edu.murdoch.ccg.yabi.util.AppDetails;
+import java.util.logging.Logger;
+import org.apache.commons.configuration.*;
+
+/**
+* A simple email sender class.
+ */
+public class MailTool {
+    
+    private static Logger logger = Logger.getLogger( AppDetails.getAppString() + "." + MailTool.class.getName());
+    private String smtpHost;
+    private String defaultRcpt;
+    private String webappName;
+    
+    public MailTool () {
+        try {
+            loadConfig();
+        } catch (Exception e) {
+            logger.severe("failed to load configuration for mail sending");
+        }
+    }
+    
+    /**
+    * Main method to send a message given on the command line.
+     */
+    public boolean sendYabiError(String message) {
+        try {
+            send(smtpHost, defaultRcpt, webappName+"@ccg.murdoch.edu.au", webappName+" error", message);
+            
+            return true;
+        } catch (Exception ex) {
+            logger.severe("failed to send message. watch for other errors, eh");
+            return false;
+        }
+    }
+    
+    private void loadConfig() throws ConfigurationException {
+        //load config details
+        Configuration conf = YabiConfiguration.getConfig();
+        smtpHost = conf.getString("smtp.host");
+        defaultRcpt = conf.getString("error.mailTo");
+        webappName = "yabi-"+conf.getString("buildname");
+    }
+    
+    /**
+     * "send" method to send the message.
+     */
+    public static void send(String smtpServer, String to, String from
+                            , String subject, String body) throws Exception {
+        Properties props = System.getProperties();
+        // -- Attaching to default Session, or we could start a new one --
+        props.put("mail.smtp.host", smtpServer);
+        Session session = Session.getDefaultInstance(props, null);
+        // -- Create a new message --
+        Message msg = new MimeMessage(session);
+        // -- Set the FROM and TO fields --
+        msg.setFrom(new InternetAddress(from));
+        msg.setRecipients(Message.RecipientType.TO,
+                          InternetAddress.parse(to, false));
+        // -- We could include CC recipients too --
+        // if (cc != null)
+        // msg.setRecipients(Message.RecipientType.CC
+        // ,InternetAddress.parse(cc, false));
+        // -- Set the subject and body text --
+        msg.setSubject(subject);
+        msg.setText(body);
+        // -- Set some other header information --
+        msg.setHeader("X-Mailer", "JavaMail");
+        msg.setSentDate(new Date());
+        // -- Send the message --
+        Transport.send(msg);
+        logger.info("Email error message dispatched to "+to);
+    }
+
+
+}
