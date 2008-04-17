0c5789c56f7f530665cedceaad22e9671adc9ed1
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2008-04-17 08:54:05 +0000

    fixing pathing issues

--- yabi/yabi-mw/src/au/edu/murdoch/ccg/yabi/util/SymLink.java
+++ yabi/yabi-mw/src/au/edu/murdoch/ccg/yabi/util/SymLink.java
@@ -11,8 +11,10 @@ public class SymLink {
     public static void createSymLink( String from, String to ) throws Exception {
         //TODO make this safe from hacking. Filenames should not have illegal characters
 
-        Runtime.getRuntime().exec("ln -s "+from+" "+to);
-        logger.info("ln -s "+from+" "+to);
+        String[] command = {"ln","-s",from,to};
+
+        Runtime.getRuntime().exec(command);
+        logger.info("ln -s '"+from+"' '"+to+"'");
     }
 
     public static void deleteSymLink( String location ) {
