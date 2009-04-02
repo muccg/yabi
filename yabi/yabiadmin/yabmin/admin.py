5dd4c3746423435b2cb6788d80d93c2415d729a7
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2009-04-02 05:15:43 +0000

    Added RslInfo and made some minor changes on the Tool page

--- yabi/yabiadmin/yabmin/admin.py
+++ yabi/yabiadmin/yabmin/admin.py
@@ -1,17 +1,33 @@
-from yabiadmin.yabmin.models import ToolType, Tool, ToolParameter, ToolGroup, ToolGrouping, ToolSet, User, FileExtension, FileType
+from yabiadmin.yabmin.models import ToolType, Tool, ToolParameter, ToolGroup, ToolGrouping, ToolSet, User, FileExtension, FileType, ParameterFilter, ParameterSwitchUse, ToolRslInfo, ToolRslExtensionModule, ToolRslArgumentOrder, ToolOutputExtension
 from django.contrib import admin
 
 class ToolGroupingInline(admin.TabularInline):
     model = ToolGrouping
     extra = 1
 
-class ToolParameterInline(admin.TabularInline):
-    model = ToolParameter
+class ToolOutputExtensionInline(admin.TabularInline):
+    model = ToolOutputExtension
     extra = 3
 
+class ToolParameterInline(admin.TabularInline):
+    model = ToolParameter
+    extra = 7
+
+class ToolRslExtensionModuleInline(admin.TabularInline):
+    model = ToolRslExtensionModule
+    extra = 3
+
+class ToolRslArgumentOrderInline(admin.TabularInline):
+    model = ToolRslArgumentOrder
+    extra = 7
+
+class ToolRslInfoAdmin(admin.ModelAdmin):
+    list_display = ['executable']
+    inlines = [ToolRslArgumentOrderInline, ToolRslExtensionModuleInline]
+
 class ToolAdmin(admin.ModelAdmin):
-    list_display = ['name', 'enabled', 'tool_groups_str', 'created_by', 'created_on']
-    inlines = [ToolParameterInline, ToolGroupingInline]
+    list_display = ['name', 'enabled', 'type', 'tool_groups_str', 'created_by', 'created_on']
+    inlines = [ToolOutputExtensionInline, ToolParameterInline, ToolGroupingInline]
 
 class ToolGroupAdmin(admin.ModelAdmin):
     list_display = ['name', 'tools_str']
@@ -27,10 +43,13 @@ class FileTypeAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 admin.site.register(FileExtension)
+admin.site.register(ParameterFilter)
+admin.site.register(ParameterSwitchUse)
 admin.site.register(ToolType)
 admin.site.register(FileType, FileTypeAdmin)
 admin.site.register(Tool, ToolAdmin)
 admin.site.register(ToolGroup, ToolGroupAdmin)
 admin.site.register(ToolSet, ToolSetAdmin)
 admin.site.register(User, UserAdmin)
+admin.site.register(ToolRslInfo, ToolRslInfoAdmin)
 
--- yabi/yabiadmin/yabmin/models.py
+++ yabi/yabiadmin/yabmin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 class ManyToManyField_NoSyncdb(models.ManyToManyField):
     def __init__(self, *args, **kwargs):
@@ -9,28 +10,28 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
-    last_modified_by = models.CharField(blank=True, max_length=50, editable=False)
-    last_modified_on = models.DateTimeField(blank=True, auto_now=True, editable=False)
+    last_modified_by = models.CharField(null=True, max_length=50, editable=False)
+    last_modified_on = models.DateTimeField(null=True, auto_now=True, editable=False)
     created_by = models.CharField(max_length=50, editable=False)
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
 
 class FileExtension(Base):
-    name = models.CharField(max_length=10, unique=True)
+    extension = models.CharField(max_length=10, unique=True)
     
     def __unicode__(self):
-        return self.name
+        return self.extension
 
 class FileType(Base):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-    extensions = models.ManyToManyField(FileExtension)
+    description = models.TextField(null=True, blank=True)
+    extensions = models.ManyToManyField(FileExtension, null=True, blank=True)
 
     def __unicode__(self):
         return self.name
 
 class ToolType(Base):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __unicode__(self):
         return self.name
@@ -38,16 +39,15 @@ class ToolType(Base):
 class Tool(Base):
     name = models.CharField(max_length=255, unique=True)
     display_name = models.CharField(max_length=255)
-    path = models.CharField(max_length=512, blank=True)
-    description = models.TextField()
+    path = models.CharField(max_length=512, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     enabled = models.BooleanField(default=True)
     type = models.ForeignKey(ToolType)
-    groups = models.ManyToManyField("ToolGroup", through='ToolGrouping')
-    output_filetypes = models.ManyToManyField(FileType, blank=True)
-    output_must_exist = models.BooleanField(default=False)
-    # I don't think this is required, it can be deduced from parameters
-    # revise
-    #input_required = models.BooleanField(default=False)
+    groups = models.ManyToManyField('ToolGroup', through='ToolGrouping', null=True, blank=True)
+    output_filetypes = models.ManyToManyField(FileExtension, through='ToolOutputExtension', null=True, blank=True)
+    file_pass_thru = models.BooleanField(default=False)
+    batch_on_param = models.ForeignKey('ToolParameter', related_name='batch_tool', null=True, blank=True)
+    batch_on_param_bundle_files = models.NullBooleanField(null=True, blank=True)
 
     def tool_groups_str(self):
         return ",".join(
@@ -60,26 +60,62 @@ class Tool(Base):
     def __unicode__(self):
         return self.name
 
-SWITCH_USE_CHOICES = (
-    ('switchOnly', 'switch only'),
-    ('valueOnly', 'value only'),
-    ('both', 'both'),
-    ('combined', 'combined'),
-    ('none', 'none'),
-)
+class ParameterSwitchUse(Base):
+    display_text = models.CharField(max_length=30)
+    value = models.CharField(max_length=20)
+    description = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.display_text
+
+class ParameterFilter(Base):
+    display_text = models.CharField(max_length=30)
+    value = models.CharField(max_length=20)
+    description = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.display_text
 
 class ToolParameter(Base):
     tool = models.ForeignKey(Tool)
-    rank = models.IntegerField(blank=True)
+    rank = models.IntegerField(null=True, blank=True)
     mandatory = models.BooleanField(blank=True, default=False)
     input_file = models.BooleanField(blank=True, default=False)
     output_file = models.BooleanField(blank=True, default=False)
-    switch = models.CharField(blank=True, max_length=5, unique=True)
-    switch_use = models.CharField(blank=True, max_length=20, choices=SWITCH_USE_CHOICES)
+    switch = models.CharField(max_length=5, null=True, blank=True, unique=True)
+    switch_use = models.ForeignKey(ParameterSwitchUse, null=True, blank=True)
     accepted_filetypes = models.ManyToManyField(FileType, blank=True)
     input_extensions = models.ManyToManyField(FileExtension, blank=True, related_name='input_params')
-    append_string = models.CharField(max_length=255, blank=True)
-    source_param = models.ForeignKey('self', blank=True)
+    filter = models.ForeignKey(ParameterFilter, null=True, blank=True)
+    filterValue = models.CharField(max_length=50, null=True, blank=True)
+    source_param = models.ForeignKey('self', related_name='source_parent', null=True, blank=True)
+    extension_param = models.ForeignKey('self', related_name='extension_parent', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.switch
+
+class ToolRslInfo(Base):
+    executable = models.CharField(max_length=50)
+    count = models.PositiveIntegerField()
+    queue = models.CharField(max_length=50, default='normal')
+    max_wall_time = models.PositiveIntegerField()
+    max_memory = models.PositiveIntegerField()
+    job_type = models.CharField(max_length=40, default='single')
+    tool = models.OneToOneField(Tool)
+
+class ToolRslExtensionModule(Base):
+    tool_rsl = models.ForeignKey(ToolRslInfo)
+    name = models.CharField(max_length=50)
+
+class ToolRslArgumentOrder(Base):
+    tool_rsl = models.ForeignKey(ToolRslInfo)
+    name = models.CharField(max_length=50)
+    rank = models.PositiveIntegerField()
+
+class ToolOutputExtension(Base):
+    tool = models.ForeignKey(Tool)
+    file_extension = models.ForeignKey(FileExtension)
+    must_exist = models.BooleanField(default=False)
 
 class ToolGroup(Base):
     name = models.CharField(max_length=100, unique=True)
