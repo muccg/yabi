b6f9eadcfe2a9b79aee2369a4e34a7e33d2d9dc5
Crispin Wellington <retrogradeorbit@gmail.com>
2009-06-03 07:08:13 +0000

    urls for static

--- yabi/yabiadmin/trunk/yabiadmin/urls.py
+++ yabi/yabiadmin/trunk/yabiadmin/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from yabiadmin.yabmin import admin as yabmin
 
+import os
 #admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include('yabiadmin.yabmin.urls')),
     (r'^admin/(.*)', admin.site.root),
+    (r'^static/(?P<path>.*)$',
+                        'django.views.static.serve', 
+                        {'document_root': os.path.join(os.path.dirname(__file__),"static"), 'show_indexes': True}),
 )
