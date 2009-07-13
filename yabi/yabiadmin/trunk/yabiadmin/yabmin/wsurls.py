8333c5591e93553281627d179e68350e57bcd7ac
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2009-07-13 05:21:31 +0000

    tool names can have . character now

--- yabi/yabiadmin/trunk/yabiadmin/yabmin/wsurls.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabmin/wsurls.py
@@ -2,8 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('yabiadmin.yabmin.wsviews',
 #    url(r'^user/(?P<user_id>\d+)/tools/$', 'user_tools', name='user_tools_view'),
-    url(r'^tool/(?P<toolname>\w+)[/]*$', 'tool', name='tool'),
+    url(r'^tool/(?P<toolname>[\w.]+)[/]*$', 'tool', name='tool'),
     url(r'^menu/(?P<username>\w+)[/]*$', 'menu', name='menu'),
+    url(r'^submitworkflow[/]*$', 'submitworkflow', name='submitworkflow'),
     url(r'^credential/(?P<username>\w+)/(?P<backend>\w+)[/]*$', 'credential', name='credential'),
     url(r'^credential/(?P<username>\w+)/(?P<backend>\w+)/(?P<detail>\w+)[/]*$', 'credential_detail', name='credential_detail'),                       
 )
