58fc43c17f0b0c8f3493601b4fcd404155bd9c26
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2009-07-23 07:07:29 +0000

    changing error address to me during devel

--- yabi/yabiadmin/trunk/yabiadmin/settings.py
+++ yabi/yabiadmin/trunk/yabiadmin/settings.py
@@ -59,8 +59,8 @@ YABIBACKEND_LIST = '/fs/ls'
 
 # make sure that this is a tuple of tuples
 ADMINS = (
-    ('Tech Alerts', 'alerts@ccg.murdoch.edu.au'),
-    #('Andrew', 'andrew@ccg.murdoch.edu.au')    
+    #('Tech Alerts', 'alerts@ccg.murdoch.edu.au'),
+    ('Andrew', 'andrew@ccg.murdoch.edu.au'),
 )
 
 LOGIN_URL = "/login"
