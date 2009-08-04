84ec3c5f7709426a8437d9917b310c441dc17c58
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2009-08-04 07:02:33 +0000

    allowing '-' char in toolnames

--- yabi/yabiadmin/trunk/yabiadmin/yabmin/wsurls.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabmin/wsurls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('yabiadmin.yabmin.wsviews',
 #    url(r'^user/(?P<user_id>\d+)/tools/$', 'user_tools', name='user_tools_view'),
-    url(r'^tool/(?P<toolname>[\w.]+)[/]*$', 'tool', name='tool'),
+    url(r'^tool/(?P<toolname>[\w.-]+)[/]*$', 'tool', name='tool'),
     url(r'^menu/(?P<username>\w+)[/]*$', 'menu', name='menu'),
     url(r'^submitworkflow[/]*$', 'submitworkflow', name='submitworkflow'),
     url(r'^credential/(?P<username>\w+)/(?P<backend>\w+)[/]*$', 'credential', name='credential'),
