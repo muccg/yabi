6941bd7ab07a87c92af84c6d06db7b86c4433b1f
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2009-08-17 07:07:03 +0000

    menu tests now in place

--- yabi/yabiadmin/trunk/yabiadmin/yabmin/tests.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabmin/tests.py
@@ -48,10 +48,25 @@ class TestYabmin(unittest.TestCase):
         c = Client()
         response = c.get('/ws/menu/andrew')
         self.assertEqual(response.status_code, 200)
-        print response.content
-##        payload = json.loads(response.content)
-##        self.assertEqual(payload["tool"]["name"], "blast.xe.ivec.org")
 
-##        # test non-existant to ensure we don't explode app
-##        response = c.get('/ws/tool/testingtool_thisshouldnotexist')
-##        self.assertEqual(response.status_code, 404)
+        payload = json.loads(response.content)
+        toolset = payload["menu"]["toolsets"][0]
+        toolgroup = toolset["toolgroups"][0]
+        tool = toolgroup["tools"][0]        
+
+        self.assertEqual(toolset["name"], "dev")
+        self.assertEqual(toolgroup["name"], "blast")
+        self.assertEqual(tool["name"], "blast.xe.ivec.org")
+        self.assertEqual(tool["displayName"], "blast")
+        self.assertEqual(tool["outputExtensions"][0], "bls")
+        self.assertEqual(tool["inputExtensions"][0], "fa")        
+
+        # test non-existant user to ensure we don't explode app
+        # menu is set up to return empty json structure but not
+        # return 404 so it does not break front end
+        response = c.get('/ws/menu/testingtool_thisshouldnotexist')
+        self.assertEqual(response.status_code, 200)
+        payload = json.loads(response.content)
+        self.assertEqual(payload["menu"]["toolsets"], [])        
+
+
