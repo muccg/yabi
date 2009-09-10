070a9c4d436afeefad5a28cccf4bdd6db9ff83ac
Crispin Wellington <retrogradeorbit@gmail.com>
2009-09-10 07:04:53 +0000

    Updating manage.py for new mango structure

--- yabi/yabiadmin/trunk/yabiadmin/manage.py
+++ yabi/yabiadmin/trunk/yabiadmin/manage.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 from django.core.management import execute_manager
+from django.conf import import_settings_module_with_extras
+
 try:
-    import settings # Assumed to be in the same directory.
+    settings = import_settings_module_with_extras('settings') # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
@@ -9,3 +11,4 @@ except ImportError:
 
 if __name__ == "__main__":
     execute_manager(settings)
+
