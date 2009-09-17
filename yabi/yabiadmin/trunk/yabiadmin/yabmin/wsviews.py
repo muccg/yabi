92fb8d12cbd02fc66d6a3053a1fbe5247a4dab8d
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2009-09-17 04:49:40 +0000

    debugging

--- yabi/yabiadmin/trunk/yabiadmin/yabmin/wsviews.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabmin/wsviews.py
@@ -78,7 +78,7 @@ def submitworkflow(request):
     logger.debug('')
     
     try:
-        logger.debug(request.POST)
+        logger.debug(request)
         # probably want to catch the type of exceptions we may get from this
         wfbuilder.build(request.POST['username'], request.POST["workflowjson"])
         
