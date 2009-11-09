72e7f151e552e92f7401d0d8d24411ecfa7313d7
Crispin Wellington <retrogradeorbit@gmail.com>
2009-11-09 09:21:18 +0000

    Wrote base class to unit test stackless tasklets

--- yabi/yabi-be-twisted/trunk/globus/test_CertificateProxy.py
+++ yabi/yabi-be-twisted/trunk/globus/test_CertificateProxy.py
@@ -1,66 +1,42 @@
-import stacklessreactor
-#stacklessreactor.install()
-
-from twisted.application.reactors import Reactor
-
-stacked = Reactor('stackless','stacklessreactor.StacklessReactor', 'Reactor that pumps the stackless loop')
-
 import CertificateProxy
 from twisted.trial import unittest, reporter, runner
 import os
 
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
+from twisted.internet.task import LoopingCall
+from twisted.python.failure import Failure
+from twisted.internet.base import _SignalReactorMixin     
 
 from test_certificates import TEST_CERT, TEST_KEY, TEST_KEY_PW
 
-class CertificateProxyTest(unittest.TestCase):
+import stackless
+from utils.stacklesstest import StacklessTest
+
+class CertificateProxyTest(StacklessTest):
     """Test the server sets up correctly and tears down correctly."""
     _suppressUpDownWarning = True
     PORT = 9000
 
-    def setUpClass(self):
+    def setUp(self):
+        StacklessTest.setUp(self)
         self.proxy = CertificateProxy.CertificateProxy()
-
+        
     def test_certificate_stores_created(self):
         """Check that the storage is created"""
         self.failUnless( os.path.exists(self.proxy.storage) )
         
     def test_unauthed_user_proxy_is_invalid(self):
         self.assert_( self.proxy.IsProxyValid('dummy_user')==False )
-        
+    
     def test_create_valid_user_proxy(self):
         """Test that we can successfuly create a user proxy cert"""
-        import stackless
-        def run():
-            self.proxy.CreateUserProxy('test_user', TEST_CERT, TEST_KEY, TEST_KEY_PW)
-            
-        from twisted.internet.task import LoopingCall
+        def _test(runner):
+            self.assert_( os.path.exists( self.proxy.ProxyFile('test_user') ) )
+            self.assert_( self.proxy.IsProxyValid('test_user') )
         
-        def call():
-            print "call"
-            stackless.schedule()
+        return self.deferred_test(self.proxy.CreateUserProxy,_test,['test_user', TEST_CERT, TEST_KEY, TEST_KEY_PW])
         
-        lc = LoopingCall(call)
-        lc.start(0.1)
-        
-        
-            
-        if True:
-            task = stackless.tasklet(run)
-            task.setup()
-            task.run()
-            
-            while task.alive:
-                pass
-                #reactor.doIteration(0.1)
-                #stackless.schedule()
-        else:
-            run()
-            
-        self.assert_( os.path.exists( self.proxy.ProxyFile('test_user') ) )
-        self.assert_( self.proxy.IsProxyValid('test_user') )
-        
-    def notest_create_invalid_user_proxy(self):
+    def test_create_invalid_user_proxy(self):
         INVALID_CERT = TEST_CERT.split()
        
         # change 20 characters randomly
@@ -70,10 +46,14 @@ class CertificateProxyTest(unittest.TestCase):
             
         INVALID_CERT = "".join(INVALID_CERT)
         
-        self.assertRaises(CertificateProxy.ProxyInitError,self.proxy.CreateUserProxy,'test_user', INVALID_CERT, TEST_KEY, TEST_KEY_PW)
+        def _test(task):
+            # make sure as exception is raised and its "CertificateProxy.ProxyInitError"
+            self.assert_( self.exception.__class__,CertificateProxy.ProxyInitError )
         
-    def notest_invalid_cert_password(self):
-        self.assertRaises(CertificateProxy.ProxyInvalidPassword,self.proxy.CreateUserProxy,'test_user', TEST_CERT, TEST_KEY, "this is an invalid password")
+        return self.deferred_test(self.proxy.CreateUserProxy,_test,['test_user', INVALID_CERT, TEST_KEY, TEST_KEY_PW])
+         
+    def test_invalid_cert_password(self):
+        return self.assertRaises(CertificateProxy.ProxyInvalidPassword,self.proxy.CreateUserProxy,'test_user', TEST_CERT, TEST_KEY, "this is an invalid password")
         
     def test_zzz_cleanup(self):
         """Test that the cleanup script works"""
