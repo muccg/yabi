e6fccd38b79e198aa321c36b90ab3b38fc1bef71
Crispin Wellington <retrogradeorbit@gmail.com>
2009-11-13 08:44:59 +0000

    default logging file clash problem fixed

--- yabi/yabi-be-twisted/trunk/log/__init__.py
+++ yabi/yabi-be-twisted/trunk/log/__init__.py
@@ -1,6 +1,7 @@
 import logging
+import os
 
-LOG_FILENAME = '/tmp/logging_example.out'
+LOG_FILENAME = '/tmp/logging_example-%d.out'%os.getpid()
 
 logging.basicConfig(filename=LOG_FILENAME,format='%(asctime)s %(levelname)s %(message)s',level=logging.DEBUG,)
 
