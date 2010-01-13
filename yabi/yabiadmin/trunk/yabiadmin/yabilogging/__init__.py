9512b432e9eddaa16a65cd9c5778035eadeaebb5
Crispin Wellington <retrogradeorbit@gmail.com>
2010-01-13 07:41:49 +0000

    fixing formatter

--- yabi/yabiadmin/trunk/yabiadmin/yabilogging/__init__.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabilogging/__init__.py
@@ -5,9 +5,7 @@ import logging, logging.handlers
 def init_logging():
     # yabiengine log
     fh = logging.handlers.TimedRotatingFileHandler(settings.LOG_DIRECTORY + '/yabiengine.log', 'midnight')
-    fmt = logging.Formatter(settings.LOGGING_FORMATTER)
-    print "FMT:",fmt
-    fh.setFormatter(fmt)
+    fh.setFormatter(logging.Formatter(settings.LOGGING_FORMATTER))
     yabienginelogger = logging.getLogger('yabiengine')
     yabienginelogger.setLevel(settings.LOGGING_LEVEL)
     yabienginelogger.addHandler(fh)
