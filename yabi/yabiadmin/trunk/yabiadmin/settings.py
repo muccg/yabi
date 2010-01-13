ddeca24ac05e4e17dbe043febc83583e6ee8b8b4
Crispin Wellington <retrogradeorbit@gmail.com>
2010-01-13 07:42:16 +0000

    fixing formatter

--- yabi/yabiadmin/trunk/yabiadmin/settings.py
+++ yabi/yabiadmin/trunk/yabiadmin/settings.py
@@ -276,7 +276,7 @@ LOG_DIRECTORY = os.path.join(PROJECT_DIRECTORY,"logs")
 LOGGING_LEVEL = DEBUG
 #LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.CRITICAL
 #LOGGING_FORMATTER = '%(asctime)s:%(name)s:%(levelname)s:%(filename)s:%(lineno)s:%(funcName)s:%(message)s'
-LOGGING_FORMATTER = "%(message)s"
+LOGGING_FORMATTER = ""
 LOGS = ['yabiengine','yabiadmin']
 
 
