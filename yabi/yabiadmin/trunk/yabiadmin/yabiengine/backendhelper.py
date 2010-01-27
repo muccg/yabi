672d1ed24192b5d7e9bd9fecd5b2701f1d9467aa
Crispin Wellington <retrogradeorbit@gmail.com>
2010-01-27 08:28:32 +0000

    work in progress

--- yabi/yabiadmin/trunk/yabiadmin/yabiengine/backendhelper.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabiengine/backendhelper.py
@@ -43,8 +43,9 @@ def get_backendcredential_for_uri(yabiusername, uri):
     # lets look at the paths for these to find candidates
     cred = None
     for bc in bcs:
-        logger.debug("path:%s bcpath:%s bc.be.path:%s"%(path,bc.homedir,bc.backend.path))
         checkpath = os.path.join(bc.backend.path,bc.homedir)
+        logger.debug("path:%s bcpath:%s bc.be.path:%s checkpath:%s"%(path,bc.homedir,bc.backend.path,checkpath))
+        
         if path.startswith(checkpath):
             # valid. If homedir path is longer than the present stored one, replace the stored one with this one to user
             if cred==None:
