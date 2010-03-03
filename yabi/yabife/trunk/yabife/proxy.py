132827f8e4e020f831c5848ec1bdbb7545f8a913
Crispin Wellington <retrogradeorbit@gmail.com>
2010-03-03 09:06:36 +0000

    test twisted proxy code

--- yabi/yabife/trunk/yabife/proxy.py
+++ yabi/yabife/trunk/yabife/proxy.py
@@ -114,9 +114,9 @@ class ProxyClient(HTTPClientProtocol,HTTPClient):
         
         self.sendCommand(self.command, self.rest)
         for header, value in self.headers.getAllRawHeaders():
-            print "SEND HEADER",header,value
             if value==['c, l, o, s, e']:
                 value = ['close']
+            print "SEND HEADER",header,value
             self.sendHeader(header, value)
             
         # now lets copy any instream down the pipola!
