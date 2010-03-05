986fa210887c3f0a849e212a5cb5a6b3c3d7f76a
Crispin Wellington <retrogradeorbit@gmail.com>
2010-03-05 09:18:18 +0000

    proxy file uploader

--- yabi/yabife/trunk/yabife/proxy.py
+++ yabi/yabife/trunk/yabife/proxy.py
@@ -56,6 +56,147 @@ def no_intr(func, *args, **kw):
 
 import StringIO
 
+from twisted.web.client import HTTPPageGetter
+
+class UploadHTTPClient(HTTPPageGetter):
+    def __init__(self):
+        self.errordata
+    
+    def sendCommand(self,command,path):
+        self.transport.write('%s %s HTTP/1.0\r\n' % (command,path) )
+        
+    def sendHeader(self,name,value):
+        if name.lower()=="connection" and value.lower()=="close":
+            return
+        return client.HTTPPageGetter.sendHeader(self,name,value)
+        
+    def rawDataReceived(self,data):
+        if int(self.status) != 200:
+            self.errordata=data
+            
+        print "RECV",data
+        
+        return HTTPPageGetter.rawDataReceived(self,data)
+        
+class UploadHTTPPageGetter(HTTPPageGetter, UploadHTTPClient):
+    pass
+
+class UploadHTTPPageDownloader(HTTPPageDownloader, UploadHTTPClient):
+    pass
+
+class UploadHTTPClientFactory(HTTPClientFactory):
+    protocol = UploadHTTPClient
+    
+    def __init__(self,url,method='GET',postdata=None, headers=None, agent="Twisted PageGetter", timeout=0, cookies=None, followRedirect=True, redirectLimit=20):
+        return HTTPClientFactory.__init__(self,url,method,postdata,headers,agent,timeout.cookies.followRedirect, redirectLimit)
+        
+    def buildProtocol(self,addr):
+        p = HTTPClientFactory.buildProtocol(self,addr)
+        return p
+        
+class UploadHTTPDownloader(UploadHTTPClientFactory):
+    """Download to a file."""
+
+    protocol = UploadHTTPPageDownloader
+    value = None
+
+    def __init__(self, url, fileOrName,
+                 method='GET', postdata=None, headers=None,
+                 agent="Twisted client", supportPartial=0,
+                 timeout=0, cookies=None, followRedirect=1,
+                 redirectLimit=20):
+        self.requestedPartial = 0
+        if isinstance(fileOrName, types.StringTypes):
+            self.fileName = fileOrName
+            self.file = None
+            if supportPartial and os.path.exists(self.fileName):
+                fileLength = os.path.getsize(self.fileName)
+                if fileLength:
+                    self.requestedPartial = fileLength
+                    if headers == None:
+                        headers = {}
+                    headers["range"] = "bytes=%d-" % fileLength
+        else:
+            self.file = fileOrName
+        UploadHTTPClientFactory.__init__(
+            self, url, method=method, postdata=postdata, headers=headers,
+            agent=agent, timeout=timeout, cookies=cookies,
+            followRedirect=followRedirect, redirectLimit=redirectLimit)
+
+
+    def gotHeaders(self, headers):
+        UploadHTTPClientFactory.gotHeaders(self, headers)
+        if self.requestedPartial:
+            contentRange = headers.get("content-range", None)
+            if not contentRange:
+                # server doesn't support partial requests, oh well
+                self.requestedPartial = 0
+                return
+            start, end, realLength = http.parseContentRange(contentRange[0])
+            if start != self.requestedPartial:
+                # server is acting wierdly
+                self.requestedPartial = 0
+
+
+    def openFile(self, partialContent):
+        if partialContent:
+            file = open(self.fileName, 'rb+')
+            file.seek(0, 2)
+        else:
+            file = open(self.fileName, 'wb')
+        return file
+
+    def pageStart(self, partialContent):
+        """Called on page download start.
+
+        @param partialContent: tells us if the download is partial download we requested.
+        """
+        if partialContent and not self.requestedPartial:
+            raise ValueError, "we shouldn't get partial content response if we didn't want it!"
+        if self.waiting:
+            try:
+                if not self.file:
+                    self.file = self.openFile(partialContent)
+            except IOError:
+                #raise
+                self.deferred.errback(failure.Failure())
+
+    def pagePart(self, data):
+        if not self.file:
+            return
+        try:
+            self.file.write(data)
+        except IOError:
+            #raise
+            self.file = None
+            self.deferred.errback(failure.Failure())
+            
+
+    def noPage(self, reason):
+        """
+        Close the storage file and errback the waiting L{Deferred} with the
+        given reason.
+        """
+        if self.waiting:
+            self.waiting = 0
+            if self.file:
+                try:
+                    self.file.close()
+                except:
+                    log.err(None, "Error closing HTTPDownloader file")
+            self.deferred.errback(reason)
+
+
+    def pageEnd(self):
+        self.waiting = 0
+        if not self.file:
+            return
+        try:
+            self.file.close()
+        except IOError:
+            self.deferred.errback(failure.Failure())
+            return
+        self.deferred.callback(self.value)
 
 
 @class_annotate
