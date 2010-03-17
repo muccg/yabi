6835ef76f77ad9c9445845ef4062284b6ab36a5b
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2010-03-17 01:11:41 +0000

    Death to that really fucking annoying 404 from favicon.

--- yabi/yabife/trunk/yabife/urls.py
+++ yabi/yabife/trunk/yabife/urls.py
@@ -1,21 +1,7 @@
 import os
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('yabife.yabifeapp.views',
-    # Example:
-    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
-
     (r'^(?P<url>ws/.*)$', 'adminproxy'),
     (r'^(?P<url>workflows/.*)$', 'storeproxy'),                       
 	(r'^[/]*$', 'design'),
@@ -36,4 +22,8 @@ urlpatterns += patterns('',
                         'django.views.static.serve', 
                         {'document_root': os.path.join(os.path.dirname(__file__),"static"), 'show_indexes': True}),
 
+)
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^favicon\.ico', 'redirect_to', {'url': '/static/images/favicon.ico'}),
 )
