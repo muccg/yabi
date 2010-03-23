dae8be48587a9e8cb955159f81c829b846d76a65
Crispin Wellington <retrogradeorbit@gmail.com>
2010-03-23 04:25:19 +0000

    + streaming ssh2 copy

--- yabi/yabi-be-twisted/trunk/ssh/ssh-copy-2.py
+++ yabi/yabi-be-twisted/trunk/ssh/ssh-copy-2.py
@@ -13,10 +13,13 @@ for delkey in ['DISPLAY','SSH_AGENT_PID','SSH_AUTH_SOCK']:
     if delkey in os.environ:
         del os.environ[delkey]
 
+def eprint(text):
+    sys.stderr.write(text)
+    sys.stderr.write("\n")
+
 SSH = "/usr/bin/ssh"
 BLOCK_SIZE = 1024
 TIMEOUT = 0.2
-FULL_TIMEOUT = 10.0
 
 L2R = 1
 R2L = 2
@@ -31,17 +34,17 @@ parser.add_option( "-R", "--remote-to-local", dest="r2l", help="force remote to 
 
 (options, args) = parser.parse_args()
 
-print "options",options
-print "args",args
+#print "options",options
+#print "args",args
 
 if len(args)!=2:
-    print "Error: Must have input and output file specified"
+    eprint("Error: Must have input and output file specified")
     sys.exit(2)
     
 infile, outfile = args
 
 if options.l2r and options.r2l:
-    print "ERROR: copy can only be remote-to-local or local-to-remote, not both"
+    eprint("ERROR: copy can only be remote-to-local or local-to-remote, not both")
     sys.exit(1)
     
 if not options.l2r and not options.r2l:
@@ -52,7 +55,7 @@ if not options.l2r and not options.r2l:
     elif re_remote.search(outfile) and not re_remote.search(infile):
         direction = L2R
     else:
-        print "ERROR: cannot guess copy direction. please specify on command line"
+        eprint("ERROR: cannot guess copy direction. please specify on command line")
         sys.exit(2)
 elif options.l2r:
     direction = L2R
@@ -67,9 +70,7 @@ if options.compress:
 if options.port:
     extra_args.extend(["-p",options.port])
     
-#password = sys.stdin.readline().rstrip('\n')
-password = "lollipop"
-print "PASS<",password,">"
+password = sys.stdin.readline().rstrip('\n')
 
 if direction == L2R:
     # 
@@ -78,23 +79,20 @@ if direction == L2R:
     hostpart, path = outfile.split(':',1)
     user, host = hostpart.split('@',1)
         
-    ssh_command = ("cat %s | /usr/bin/ssh "+(" ".join(extra_args))+" %s@%s"%(user,host)+" 'cat>\"%s\" '")%(infile,path)
+    ssh_command = ("cat %s | %s "+(" ".join(extra_args))+" %s@%s"%(user,host)+" 'cat>\"%s\" '")%(infile,SSH,path)
     command = '/bin/bash -c "'+ssh_command+'"'
-    print command
     
     child = pexpect.spawn(command)
-    child.setecho(False)
-    child.logfile_read = sys.stdout
     res = 0
     while res!=2:
         res = child.expect(["passphrase for key .+:","password:", "Permission denied",pexpect.EOF,pexpect.TIMEOUT],timeout=TIMEOUT)
         if res<=1:
             # send password
-            print "sending",password
+            #print "sending",password
             child.sendline(password)
         elif res==2:
             # password failure
-            print "Access denied"
+            eprint("Access denied")
             sys.exit(1)
             
         elif res==3:
@@ -103,15 +101,10 @@ if direction == L2R:
             if child.isalive():
                 child.wait()
             
-            print "RESULT",child.exitstatus
             sys.exit(child.exitstatus)
         
-        elif res==4:
-            # EOF
-            pass
-        
-        elif res==5:
-            # Timeout
+        else:
+            # EOF or timeout
             pass
         
 elif direction == R2L:
@@ -121,25 +114,16 @@ elif direction == R2L:
     hostpart, path = infile.split(':',1)
     user, host = hostpart.split('@',1)
     
-    ssh_command = ("/usr/bin/ssh "+(" ".join(extra_args))+" %s@%s"%(user,host)+" 'cat \"%s\"' > '%s'")%(path,outfile)
+    ssh_command = ("%s "+(" ".join(extra_args))+" %s@%s"%(user,host)+" 'cat \"%s\"' > '%s'")%(SSH,path,outfile)
     command = '/bin/bash -c "'+ssh_command+'"'
-    print command
     
     child = pexpect.spawn(command)
-    child.logfile_read = StringIO.StringIO()
-    data_flow = False                           # once dataflow begins, set this to true
-    timeout_count = 0                           # how many times weve hit a timeout
     res = 0
     while res!=2:
         res = child.expect(["passphrase for key .+:","password:","Permission denied",pexpect.EOF,pexpect.TIMEOUT],timeout=TIMEOUT)
-        print "RES:",res
         if res<=1:
             # send password
-            print "sending",password
             child.sendline(password)
-            child.logfile_read = StringIO.StringIO()            # reset data buffer
-            
-            data_flow = True
             
         elif res==3:
             child.delaybeforesend=0
@@ -147,30 +131,15 @@ elif direction == R2L:
             if child.isalive():
                 child.wait()
             
-            print "RESULT",child.exitstatus
             sys.exit(child.exitstatus)
             
-        elif res==5:
-            if data_flow:
-                if timeout_count > FULL_TIMEOUT/TIMEOUT:
-                    print "Access denied"
-                    sys.exit(2)
-                else:
-                    timeout_count += 1
         elif res==2:
             # password failure!
             print "Access denied"
             sys.exit(1)
-        elif res==4:
-            # EOF. the whole file may have been delivered just then!
-            data = child.logfile_read.getvalue()
-            assert data[0:2]=="\r\n", "echod back carridge return from password input is missing!"
-                        
-            fh = open(outfile,'wb')
-            fh.write(data[2:].replace("\r\n","").replace(" ","").replace("-BEGIN-","").replace("-END-",""))
-            fh.close()
-            child.sendeof()
-            sys.exit(0)
+            
+        else:
+            pass
         
             
           
