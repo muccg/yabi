669cfb6c6bcc49ec893bc8dfbf99abb4198e631a
Crispin Wellington <retrogradeorbit@gmail.com>
2010-03-24 08:28:25 +0000

    bugfixing

--- yabi/yabi-be-twisted/trunk/ssh/SSHRun.py
+++ yabi/yabi-be-twisted/trunk/ssh/SSHRun.py
@@ -30,7 +30,7 @@ class SSHRun(BaseShell):
     ssh_exec = os.path.join( os.path.dirname(os.path.realpath(__file__)), "ssh-exec.py" )
     python = "/usr/bin/python"
     
-    def run(self, certfile, remote_command="hostname", username="yabi", host="faramir.localdomain", working="/tmp", port="22", stdout="STDOUT.txt", stderr="STDERR.txt"):
+    def run(self, certfile, remote_command="hostname", username="yabi", host="faramir.localdomain", working="/tmp", port="22", stdout="STDOUT.txt", stderr="STDERR.txt",password=""):
         """Spawn a process to run a remote ssh job. return the process handler"""
         subenv = self._make_env()
         
