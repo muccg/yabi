b204249dceb8c93807de49519559a5f5b7ac71b1
Crispin Wellington <retrogradeorbit@gmail.com>
2010-04-06 03:58:49 +0000

    keystore destruction code

--- yabi/yabi-be-twisted/trunk/ssh/KeyStore.py
+++ yabi/yabi-be-twisted/trunk/ssh/KeyStore.py
@@ -1,19 +1,34 @@
-# -*- coding: utf-8 -*-
+`# -*- coding: utf-8 -*-
 
 import tempfile, os, weakref
+
+def rm_rf(root):
+    for path, dirs, files in os.walk(root, False):
+        for fn in files:
+            os.unlink(os.path.join(path, fn))
+        for dn in dirs:
+            os.rmdir(os.path.join(path, dn))
+    os.rmdir(root)
 
 class KeyStore(object):
     def __init__(self, path=None, expiry=60):
         if path:
-            self.path = path
+            self.directory = path
         else:
             # make a temporary storage directory
             self.directory = tempfile.mkdtemp(suffix=".ssh",prefix="",dir=None)
 
         self.keys = {}
         
-
-    def save_identity(self, identity):
+    def __del__(self):
+        self.clear_keystore()
+        
+    def clear_keystore(self):
+        assert os.path.exists(self.directory), "Can't clear keystore that doesn't exist on disk"
+        
+        rm_rf(self.directory)
+        
+    def save_identity(self, identity, tag=None):
         filename = tempfile.mktemp()
         path = os.path.join( self.directory, filename )
         fh = open( path, "w" )
@@ -22,7 +37,15 @@ class KeyStore(object):
         
         os.chmod( filename, 0600 )
         
+        if tag:
+            self.keys[tag] = filename
+        
         return filename
+        
+    def delete_identity(self, tag):
+        os.unlink(self.keys[tag])
+        del self.keys[tag]
+        
         
     
        
