eb53d948505eb29f1c64decb3bb57c727868fb63
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2010-04-22 07:22:57 +0000

    model for capturing registration data

--- yabi/yabife/trunk/yabife/djopenid/consumer/models.py
+++ yabi/yabife/trunk/yabife/djopenid/consumer/models.py
@@ -18,6 +18,37 @@ class UserOpenID(models.Model):
         search_fields = ['user']
         list_display = ('user', 'openid_url')
 
+class OpenIDRegistration(models.Model):
+    name = models.TextField(null=True, blank=True)
+    openid_url = models.TextField(null=True, blank=True)
+    organisation = models.TextField(null=True, blank=True)
+    faculty = models.TextField(null=True, blank=True)
+    user_type = models.TextField(null=True, blank=True)
+    org_user_id = models.TextField(null=True, blank=True)
+    email = models.TextField(null=True, blank=True)
+    contact_number = models.TextField(null=True, blank=True)
+    supervisor_name = models.TextField(null=True, blank=True)
+    supervisor_number = models.TextField(null=True, blank=True)
+    supervisor_email = models.TextField(null=True, blank=True)
+    project_title = models.TextField(null=True, blank=True)
+    project_description = models.TextField(null=True, blank=True)
+    rfcd_code_1 = models.TextField(null=True, blank=True)
+    rfcd_code_1_weight = models.TextField(null=True, blank=True)
+    rfcd_code_2 = models.TextField(null=True, blank=True)
+    rfcd_code_2_weight = models.TextField(null=True, blank=True)
+    rfcd_code_3 = models.TextField(null=True, blank=True)
+    rfcd_code_3_weight = models.TextField(null=True, blank=True)
+    resources_compute = models.TextField(null=True, blank=True)
+    resources_data = models.TextField(null=True, blank=True)
+    resources_network = models.TextField(null=True, blank=True)
+    estimate = models.TextField(null=True, blank=True)
+    describe = models.TextField(null=True, blank=True)
+    software_needs = models.TextField(null=True, blank=True)
+    agreement = models.TextField(null=True, blank=True)
+
+    class Admin:
+        search_fields = ['name', 'email']
+        list_display = ('name', 'openid_url', 'organisation', 'email')    
 
 class OpenIDBackend:
     """
