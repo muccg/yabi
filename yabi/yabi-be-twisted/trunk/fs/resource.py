53970be420ba807f0a41b4cbdec59a085d760682
Crispin Wellington <retrogradeorbit@gmail.com>
2010-05-05 06:27:48 +0000

    make sub segments pass through to uploader

--- yabi/yabi-be-twisted/trunk/fs/resource.py
+++ yabi/yabi-be-twisted/trunk/fs/resource.py
@@ -74,7 +74,6 @@ class FSResource(resource.Resource, BackendResource):
         elif segments[0]=="ticket":
             return UploadTicket(request, segments, fsresource=self), []
         elif segments[0]=="upload":
-            return FileUploadResource(request, segments, fsresource=self), []
-        
-        
+            return FileUploadResource(request, segments, fsresource=self), segments[1:]
+                
         return resource.Resource.locateChild(self,request,segments)
