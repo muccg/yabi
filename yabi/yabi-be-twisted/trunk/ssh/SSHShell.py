ea2f83636aa7428455c3aadb13e4e2b10eadb55f
Crispin Wellington <retrogradeorbit@gmail.com>
2010-05-11 06:57:11 +0000

    attempt to fix SSHShell obscure quoting bug

--- yabi/yabi-be-twisted/trunk/ssh/SSHShell.py
+++ yabi/yabi-be-twisted/trunk/ssh/SSHShell.py
@@ -27,6 +27,7 @@ class SSHShell(BaseShell):
     def _make_echo(self,filename):
         """Turn a filename into the remote eval line"""
         filename = '"'+filename.replace('"','\\"')+'"'
+        print "FNAME:",filename
         return '`echo -e \'%s\'`'%(convert_filename_to_encoded_for_echo(filename))
 
     def execute(self, certfile, host, command, username, password, port=22):
