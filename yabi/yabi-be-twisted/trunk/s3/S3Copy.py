414adf2d50766dc22b4ace1d30d5657b712c0669
Crispin Wellington <retrogradeorbit@gmail.com>
2010-08-10 09:52:00 +0000

    helpers for s3

---
+++ yabi/yabi-be-twisted/trunk/s3/S3Copy.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""Implement scp connections"""
+
+import os
+import tempfile
+
+from FifoPool import Fifos
+
+from BaseShell import BaseShell, BaseShellProcessProtocol
+
+class S3ProcessProtocol(BaseShellProcessProtocol):
+    def __init__(self, password):
+        BaseShellProcessProtocol.__init__(self)
+        self.started = False
+        self.password = password
+        
+    def connectionMade(self):
+        #print "Connection made"
+        self.transport.write(str(self.password))
+        self.transport.write("\n")
+        self.transport.closeStdin()
+        self.started = True
+                
+    def isStarted(self):
+        return self.started
+        
+class S3Error(Exception):
+    pass
+
+class S3Copy(BaseShell):
+    scp = os.path.join( os.path.dirname(os.path.realpath(__file__)), "s3-copy.py" )
+    python = "/usr/bin/python"
+    
+    def WriteToRemote(self, certfile, remoteurl, password="",fifo=None):
+        subenv = self._make_env()
+        
+        if not fifo:
+            fifo = Fifos.Get()
+            
+        remotehost,remotepath = remoteurl.split(':',1)
+            
+        command = [   self.python, self.scp,
+                "-i",certfile,              # keyfile
+                "-P","22",                  # port
+                fifo,                       # localfile
+                remoteurl
+            ]
+        print "C:",command
+            
+        return BaseShell.execute(self,SCPProcessProtocol(password),
+            command
+        ), fifo
+        
+    def ReadFromRemote(self,certfile,remoteurl,password="",fifo=None):
+        subenv = self._make_env()
+        
+        if not fifo:
+            fifo = Fifos.Get()
+            
+        return BaseShell.execute(self,SCPProcessProtocol(password),
+            [   self.python, self.scp,
+                "-i",certfile,
+                "-P","22",
+                remoteurl,
+                fifo
+            ]
+        ), fifo
---
+++ yabi/yabi-be-twisted/trunk/s3/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import BaseShell
+import KeyStore
+import SSHAuth
+import SSHShell
+import SSHCopy
+import SSHRun
+
+# module level "singletons"
+Copy = SSHCopy.SSHCopy()
+Run = SSHRun.SSHRun()
+Shell = SSHShell.SSHShell()
+
