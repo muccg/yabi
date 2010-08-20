522210f37163354b8bcfd05173bc403ff1b67080
Crispin Wellington <retrogradeorbit@gmail.com>
2010-08-20 09:43:35 +0000

    debug

--- yabi/yabi-be-twisted/trunk/fs/FileGetResource.py
+++ yabi/yabi-be-twisted/trunk/fs/FileGetResource.py
@@ -104,6 +104,10 @@ class FileGetResource(resource.PostableResource):
             import fcntl, errno
             fcntl.fcntl(file.fileno(), fcntl.F_SETFL, os.O_NONBLOCK) 
             
+            for i in range(20):
+                #time.sleep(1)
+                stackless.schedule()
+            
             # datastream stores whether we have sent an ok response code yet
             datastream = False
             
