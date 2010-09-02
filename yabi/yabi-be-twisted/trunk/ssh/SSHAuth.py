44205909f70223dc9e4f26b56b29a7c6f1e587b3
Crispin Wellington <retrogradeorbit@gmail.com>
2010-09-02 06:14:39 +0000

    connection failed chew through

--- yabi/yabi-be-twisted/trunk/ssh/SSHAuth.py
+++ yabi/yabi-be-twisted/trunk/ssh/SSHAuth.py
@@ -42,4 +42,4 @@ class SSHAuth(object):
             raise NoCredentials( "User: %s does not have credentials for this backend %s on host %s\n"%(username,scheme,hostname) )
         
         except ConnectionFailure, cf:
-            raise AuthException( "Tried to get credentials from %s:%d and failed: %s"%(config.yabiadminserver,config.yabiadminport,gf.args[0] ) )
+            raise AuthException( "Tried to get credentials from %s:%d and failed: %s"%(config.yabiadminserver,config.yabiadminport,cf.args[0] ) )
