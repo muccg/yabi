02118e6772b47afc67b2fa6d87b966ceb54c6b68
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2010-09-03 01:14:07 +0000

    Set session expiry to 1 hour.

--- yabi/yabife/trunk/yabife/settings.py
+++ yabi/yabife/trunk/yabife/settings.py
@@ -156,6 +156,7 @@ MIDDLEWARE_CLASSES = (
 # our session cookie name (set to be different to admin)
 SESSION_COOKIE_PATH = url('/')
 SESSION_SAVE_EVERY_REQUEST = True
+SESSION_COOKIE_AGE = 60 * 60 # 1 hour
 
 ROOT_URLCONF = 'yabife.urls'
 
