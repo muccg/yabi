99761c1fc87e41ec7f5b3fa778c8682c073bed22
Crispin Wellington <retrogradeorbit@gmail.com>
2010-09-07 08:31:15 +0000

    debug

--- yabi/yabiadmin/trunk/yabiadmin/yabiengine/tasks.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabiengine/tasks.py
@@ -4,6 +4,7 @@ from yabiadmin.yabiengine.enginemodels import EngineWorkflow
 
 @task
 def build(workflow_id):
+    print "BUILD:",workflow_id
     assert(workflow_id)
     eworkflow = EngineWorkflow.objects.get(id=workflow_id)
     eworkflow.build()
@@ -12,6 +13,7 @@ def build(workflow_id):
 
 @task
 def walk(workflow_id):
+    print "WALK:",workflow_id
     assert(workflow_id)
     eworkflow = EngineWorkflow.objects.get(id=workflow_id)
     eworkflow.walk()
