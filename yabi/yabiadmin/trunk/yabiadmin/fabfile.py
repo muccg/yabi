a14402b8b23a9b13ab0fa309841216ba23bf19ac
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2010-09-24 08:23:42 +0000

    munge settings for target

--- yabi/yabiadmin/trunk/yabiadmin/fabfile.py
+++ yabi/yabiadmin/trunk/yabiadmin/fabfile.py
@@ -31,12 +31,16 @@ def deploy():
     Make a user deployment
     """
     _ccg_deploy_user()
+    env.settings_file = env.app_root + env.app_name + "/" + os.environ["USER"] + "/" + env.app_name + "/settings.py"
+    print local("sed -i.bak -r -e 's/<CCG_TARGET_NAME>/%s/g' %s"  % (os.environ["USER"], env.settings_file))
 
 def snapshot():
     """
     Make a snapshot deployment
     """
     _ccg_deploy_snapshot()
+    env.settings_file = env.app_root + env.app_name + "/snapshot/" + env.app_name + "/settings.py"
+    print local("sed -i.bak -r -e 's/<CCG_TARGET_NAME>/%s/g' %s"  % ("snapshot", env.settings_file))
 
 def release():
     """
