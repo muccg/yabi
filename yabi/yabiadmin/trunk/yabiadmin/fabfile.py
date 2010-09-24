b4ebe557b6ae19ebab220331272646a403dccf80
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2010-09-24 07:41:42 +0000

    initial fabfile for running celeryd from within a virtual python installation and using celery2

--- yabi/yabiadmin/trunk/yabiadmin/fabfile.py
+++ yabi/yabiadmin/trunk/yabiadmin/fabfile.py
@@ -1,5 +1,6 @@
-from fabric.api import env
+from fabric.api import env, local
 from ccgfab.base import *
+import os
 
 env.app_root = '/usr/local/python/ccgapps/'
 env.app_name = 'yabiadmin'
@@ -13,11 +14,24 @@ env.writeable_dirs.extend([]) # add directories you wish to have created and mad
 env.content_excludes.extend([]) # add quoted patterns here for extra rsync excludes
 env.content_includes.extend([]) # add quoted patterns here for extra rsync includes
 
+# /usr/local/python26/ccgapps/yabiadmin/ahunter/yabiadmin/virtualpython/bin/celeryd
+env.celeryd = env.app_root + 'yabiadmin/ahunter/yabiadmin/virtualpython/bin/celeryd'
+env.celeryd_options = " -l debug"
+#env.celeryd_options = " -h"
+env.django_project_dir = env.app_root + 'yabiadmin/ahunter/yabiadmin/'
+env.django_parent_dir = env.app_root + 'yabiadmin/ahunter/'
+env.virtual_python="/usr/local/python26/ccgapps/yabiadmin/ahunter/yabiadmin/virtualpython/bin/python"
+
 def deploy():
     """
     Make a user deployment
     """
     _ccg_deploy_user()
+
+    print "**********************************************************"
+    print "* Dont forget your celeryd!"
+    print "* Usage: ./init_scripts/centos/celeryd {start|stop|restart|}"
+    print "**********************************************************"
 
 def snapshot():
     """
@@ -48,3 +62,43 @@ def purge_snapshot():
     Remove the snapshot deployment
     """
     _ccg_purge_snapshot()
+
+def celeryd():
+    #os.environ["DJANGO_SETTINGS_MODULE"]="yabiadmin.settings"
+    os.environ["DJANGO_SETTINGS_MODULE"]="settings"
+    os.environ["DJANGO_PROJECT_DIR"]=env.django_project_dir
+    os.environ["CELERY_LOADER"]="django"
+    os.environ["CELERY_CHDIR"]=env.django_project_dir
+    os.environ["PYTHONPATH"] = "/usr/local/etc/ccgapps/:" + env.django_project_dir + ":" + env.django_parent_dir
+    os.environ["PROJECT_DIRECTORY"] = "/usr/local/python/ccgapps/yabiadmin/ahunter/yabiadmin/"
+
+    print local(env.virtual_python + " " + env.celeryd + env.celeryd_options, capture=False)
+
+def syncdb():
+    _manage("syncdb")
+
+def _manage(opt=help):
+    #os.environ["DJANGO_SETTINGS_MODULE"]="yabiadmin.settings"
+    os.environ["DJANGO_SETTINGS_MODULE"]="settings"
+    os.environ["DJANGO_PROJECT_DIR"]=env.django_project_dir
+    os.environ["CELERY_LOADER"]="django"
+    os.environ["CELERY_CHDIR"]=env.django_project_dir
+    os.environ["PYTHONPATH"] = "/usr/local/etc/ccgapps/:" + env.django_project_dir + ":" + env.django_parent_dir
+    os.environ["PROJECT_DIRECTORY"] = "/usr/local/python/ccgapps/yabiadmin/ahunter/yabiadmin/"
+
+    print local(env.virtual_python + " " + env.django_project_dir + "/manage.py " + opt, capture=False)
+
+
+
+"""
+export DJANGODEV=1
+export DJANGO_SETTINGS_MODULE="yabiadmin.settings"
+export DJANGO_PROJECT_DIR="/usr/local/python/ccgapps/yabiadmin/ahunter/"
+export PROJECT_DIRECTORY="/usr/local/python/ccgapps/yabiadmin/ahunter/"
+export CELERYD_CHDIR="/usr/local/python/ccgapps/yabiadmin/ahunter/"
+export PYTHONPATH="/usr/local/python/ccgapps/yabiadmin/ahunter/"
+export PYTHONPATH=$PYTHONPATH:/usr/local/etc/ccgapps/
+export CELERY_LOADER="django"
+
+"""
+
