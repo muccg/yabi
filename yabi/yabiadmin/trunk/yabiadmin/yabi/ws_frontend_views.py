7b4a57fb7d710c11e0ef65f085cced23aa61a2b3
Crispin Wellington <retrogradeorbit@gmail.com>
2010-10-05 08:56:21 +0000

    wrong variable name

--- yabi/yabiadmin/trunk/yabiadmin/yabi/ws_frontend_views.py
+++ yabi/yabiadmin/trunk/yabiadmin/yabi/ws_frontend_views.py
@@ -264,7 +264,7 @@ def submitworkflow(request):
 
     # put the workflow in the store
     from yabiadmin.yabistoreapp import db
-    db.save_workflow(user.name, workflow.workflow_id, json, workflow.status, workflow.name)
+    db.save_workflow(user.name, workflow.workflow_id, workflow_json, workflow.status, workflow.name)
     logger.debug("db.save_workflow(%s,%s,%s,%s,%s)"%(user.name,workflow.workflow_id, json, workflow.status, workflow.name))
     
     # trigger a build via celery
