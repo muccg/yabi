7d1b2632598cd6284021668cdc8fb04e9cb16feb
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2010-10-21 06:43:01 +0000

    - Switches to argparser.   - Adds yabi arguments: --yabi-debug, --yabi-bg   - Adds POST ability to Yabi class   - Makes Login code into an Action   - Adds RemoteAction to run jobs on the clusters   - Adds Attach capablity - attach to a remote job and wait until it is completed then display stdout and stderr   - Defaults job running to foreground.   - Changes ps to jobs - output changed to display all jobs with their status

--- yabi/yabi-sh/trunk/actions.py
+++ yabi/yabi-sh/trunk/actions.py
@@ -1,21 +1,126 @@
 import time
 import json
 import os
+import sys
+
+from yabish import RemoteError
 
 class Action(object):
-    def __init__(self, yabi):
+    def __init__(self, yabi, name=None):
         self.yabi = yabi
+        self.name = name
+        if self.name is None:
+            self.name = self.__class__.__name__.lower()
 
     def process(self, args):
         params = self.map_args(args)
         resp, json_response = self.yabi.get(self.url, params)
-        self.process_response(self.decode_json(json_response))
+        return self.process_response(self.decode_json(json_response))
 
     def decode_json(self, resp):
         return json.loads(resp)        
 
+class FileDownload(object):
+    '''Mix into an Action that requires downloading files'''
+    get_url = 'ws/fs/get'
 
-class LS(Action):
+    def download_file(self, uri, dest):
+        params = {'uri': uri}
+        response, contents = self.yabi.get(self.get_url, params)
+        if hasattr(dest, 'write'):
+            dest.write(contents)
+        else:
+            with open(dest, 'w') as f:
+                f.write(contents)
+
+class RemoteAction(Action):
+    def __init__(self, *args, **kwargs):
+        Action.__init__(self, *args, **kwargs)
+        self.url = 'ws/yabish/submitjob'
+
+    def process(self, args):
+        params = {'name': self.name}
+        for i,arg in enumerate(args):
+            params['arg' + str(i)] = arg
+        resp, json_response = self.yabi.post(self.url, params)
+        decoded_resp = self.decode_json(json_response)
+        if not decoded_resp['success']:
+            raise RemoteError(decoded_resp['msg'])
+        return decoded_resp['workflow_id']
+
+class Attach(Action, FileDownload):
+    def __init__(self, *args, **kwargs):
+        Action.__init__(self, *args, **kwargs)
+        self.url = 'workflows/'
+
+    def sleepgenerator(self):
+        t = 1
+        while t < 30:
+            yield t
+            t *= 2
+        while True:
+            yield 30
+
+    def get_workflow(self, workflow_id):
+        url = self.url + str(workflow_id)
+        resp, json_response = self.yabi.get(url)
+        return self.decode_json(json_response)
+
+    def process(self, args):
+        assert len(args) == 1, "Workflow id should be passed in"
+        gen = self.sleepgenerator()
+        workflow_id = args[0]
+        resp = {}
+        status = ''
+        while status.lower() != 'complete':
+            try:
+                resp = self.get_workflow(workflow_id)
+                status = resp.get('status', '')
+                time.sleep(gen.next())
+            except KeyboardInterrupt:
+                print
+                print "Job %s current status is '%s'" % (workflow_id, status)
+                sys.exit()
+        stageout_dir = resp['json']['jobs'][0]['stageout']
+        stdout = os.path.join(stageout_dir,'STDOUT.txt')
+        stderr = os.path.join(stageout_dir,'STDERR.txt')
+        self.download_file(stdout, sys.stdout)
+        self.download_file(stderr, sys.stderr)
+
+class ForegroundRemoteAction(object):
+    def __init__(self, *args, **kwargs):
+        self.action = RemoteAction(*args, **kwargs)
+        self.attacher = Attach(*args, **kwargs)
+
+    def process(self, args):
+        workflow_id = self.action.process(args)
+        self.attacher.process([workflow_id])
+
+class BackgroundRemoteAction(object):
+    def __init__(self, *args, **kwargs):
+        self.action = RemoteAction(*args, **kwargs)
+
+    def process(self, args):
+        workflow_id = self.action.process(args)
+        print "Submitted. Workflow id: %s" % workflow_id
+
+class Login(Action):
+    def __init__(self, *args, **kwargs):
+        Action.__init__(self, *args, **kwargs)
+        self.url = 'wslogin'
+
+    def map_args(self, args):
+        return {'username': args[0], 'password': args[1]}
+
+    def process(self, args):
+        params = self.map_args(args)
+        resp, json_response = self.yabi.post(self.url, params)
+        return self.process_response(self.decode_json(json_response))
+
+    def process_response(self, response):
+        return response.get('success', False)
+
+class Ls(Action):
     def __init__(self, *args, **kwargs):
         Action.__init__(self, *args, **kwargs)
         self.url = 'ws/fs/ls'
@@ -38,10 +143,10 @@ class LS(Action):
         for f in files:
             print f[0]
 
-class PS(Action):
+class Jobs(Action):
     def __init__(self, *args, **kwargs):
         Action.__init__(self, *args, **kwargs)
-        self.url = 'workflows/tszabo/datesearch'
+        self.url = 'workflows/datesearch'
 
     def map_args(self, args):
         if args and args[0]:
@@ -52,23 +157,17 @@ class PS(Action):
         return {'start': start}
 
     def process_response(self, response):
-        for job in [j for j in response if j['status'].lower() in ('', 'running')]:
-            print '%7d  %s  %s' % (job['id'], job['created_on'], job['name'])
+        #for job in [j for j in response if j['status'].lower() in ('', 'running')]:
+        for job in response:
+            print '%7d  %s  %10s  %s' % (job['id'], job['created_on'], job['status'].upper(), job['name'])
 
-class CP(Action):
+class Cp(Action, FileDownload):
     def __init__(self, *args, **kwargs):
         Action.__init__(self, *args, **kwargs)
         self.url = 'ws/fs/copy'
-        self.get_url = 'ws/fs/get'
 
     def is_local_file(self, filename):
         return (filename.find(':') == -1)
-
-    def download_file(self, uri, name):
-        params = {'uri': uri}
-        response, contents = self.yabi.get(self.get_url, params)
-        with open(name, 'w') as f:
-            f.write(contents)
 
     def process(self, args):
         src = args[0]
@@ -86,7 +185,7 @@ class CP(Action):
             params = {'src': src, 'dst': dst}
             self.yabi.get(self.url, params)
 
-class RM(Action):
+class Rm(Action):
     def __init__(self, *args, **kwargs):
         Action.__init__(self, *args, **kwargs)
         self.url = 'ws/fs/rm'
--- yabi/yabi-sh/trunk/yabish.py
+++ yabi/yabi-sh/trunk/yabish.py
@@ -2,38 +2,62 @@
 
 import sys
 import readline
+import json
+from argparse import ArgumentParser
 
 from yaphc import Http, UnauthorizedError, PostRequest, GetRequest
 import actions
 
-# TODO env variable
+# TODO config file
+YABI_URL = 'https://faramir/yabife/tszabo/'
 
-DEBUG = True
-YABI_URL = 'https://faramir:19443/yabi/'
+class CommunicationError(StandardError):
+    def __init__(self, status_code, url, response):
+        self.status_code = status_code
+        self.url = url
+        self.response = response
+
+    def __str__(self):
+        return "%s - %s" % (self.status_code, self.url)
+
+class RemoteError(StandardError):
+    pass
 
 def main():
-    yabi = Yabi()
+    debug = False
+    yabi = None
     try:
-        args = CommandLineArguments(sys.argv)
+        argparser = ArgumentParser(description='YABI shell', add_help=False)
+        argparser.add_argument("--yabi-debug", action='store_true', help="Run in debug mode")
+        argparser.add_argument("--yabi-bg", action='store_true', help="Run in background")
+        options, args = argparser.parse_known_args()        
 
-        if args.no_arguments:
+        args = CommandLineArguments(args)
+
+        if args.no_arguments or args.first_argument in ('-h', '--help'):
             print_usage()
             return
+
+        yabi = Yabi(bg=options.yabi_bg, debug=options.yabi_debug)
         action = yabi.choose_action(args.first_argument)
         action.process(args.rest_of_arguments)
+
     except Exception, e:
-        print_error(e)
-        if DEBUG:
-            print '-' * 5 + ' DEBUG ' + '-' * 5
-            raise
+        print_error(e, debug)
     finally:
-        yabi.session_finished()
+        if yabi is not None:
+            yabi.session_finished()
 
 class Yabi(object):
-    def __init__(self):
+    def __init__(self, bg=False, debug=False):
         self.http = Http(base_url=YABI_URL)
         self.username = None
-       
+        self.run_in_background = bg
+        self.debug = debug
+        if self.debug:
+            import httplib2
+            httplib2.debuglevel = 1
+ 
     def login(self):
         import getpass
         system_user = getpass.getuser()
@@ -41,29 +65,48 @@ class Yabi(object):
         if '' == username.strip():
             username = system_user
         password = getpass.getpass()
-        login_url = "login"
-        request = PostRequest('login', params= {
-            'username': username, 'password': password})
-        # TODO change to call web service login and check result
-        self.http.make_request(request)
-        self.username = username
+        login_action = actions.Login(self)
+        return login_action.process([username, password])
 
-    def get(self, url, params):
+    def request(self, method, url, params=None):
+        if params is None:
+            params = {}
         try:
-            request = GetRequest(url, params)
+            if method == 'GET':
+                request = GetRequest(url, params)
+            elif method == 'POST':
+                request = PostRequest(url, params)
+            else:
+                assert False, "Method should be GET or POST"
+            if self.debug:
+                print '=' * 5 + 'Making HTTP request'
             resp, contents = self.http.make_request(request)
+            if self.debug:
+                print '=' * 5 + 'End of HTTP request'
         except UnauthorizedError:
-            self.login()
+            if not self.login():
+                raise StandardError("Invalid username/password")
             resp, contents = self.http.make_request(request)
+        if resp.status >= 400:
+            raise CommunicationError(resp.status, url, contents)
         return resp, contents
 
+    def get(self, url, params=None):
+        return self.request('GET', url, params)
+
+    def post(self, url, params=None):
+        return self.request('POST', url, params)
+
     def choose_action(self, action_name):
-        class_name = action_name.upper()
+        class_name = action_name.capitalize()
         try:
-            cls = getattr(sys.modules['actions'], action_name.upper())
+            cls = getattr(sys.modules['actions'], class_name)
         except AttributeError:
-            raise StandardError('Unsupported action: ' + action_name)
-        return cls(self)
+            if self.run_in_background:
+                cls = actions.BackgroundRemoteAction
+            else:
+                cls = actions.ForegroundRemoteAction
+        return cls(self, name=action_name)
 
     def session_finished(self):
         self.http.finish_session()
@@ -75,24 +118,28 @@ BLA BLA BLA
 Command should be used like BLA BLA BLA
 '''
 
-def print_error(error):
-    print 'An error occured: \n\t%s' % error
+def print_error(error, debug=False):
+    print >> sys.stderr, 'An error occured: \n\t%s' % error
+    if debug:
+        print >> sys.stderr, '-' * 5 + ' DEBUG ' + '-' * 5
+        import traceback
+        traceback.print_exc()
 
 class CommandLineArguments(object):
-    def __init__(self, argv):
-        self.argv = argv
+    def __init__(self, args):
+        self.args = args
 
     @property
     def no_arguments(self):
-        return len(sys.argv) == 1
+        return len(self.args) == 0
 
     @property
     def first_argument(self):
-        return self.argv[1]
+        return self.args[0]
 
     @property
     def rest_of_arguments(self):
-        return [] if len(self.argv) <= 2 else self.argv[2:]
+        return [] if len(self.args) <= 1 else self.args[1:]
 
 if __name__ == "__main__":
     main()
