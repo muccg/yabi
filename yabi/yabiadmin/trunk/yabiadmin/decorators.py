7922d4b6760971f2e1610f8aafe83b131a38a68b
Crispin Wellington <retrogradeorbit@gmail.com>
2010-10-28 06:57:45 +0000

    new debug decorator

--- yabi/yabiadmin/trunk/yabiadmin/decorators.py
+++ yabi/yabiadmin/trunk/yabiadmin/decorators.py
@@ -58,3 +58,35 @@ def authentication_required(f):
             return HttpResponseUnauthorized()
         return f(*args, **kwargs)
     return new_function
+    
+# Number of times to indent output
+# A list is used to force access by reference
+__report_indent = [0]
+
+def report(fn):
+    """Decorator to print information about a function
+    call for use while debugging.
+    Prints function name, arguments, and call number
+    when the function is called. Prints this information
+    again along with the return value when the function
+    returns.
+    """
+
+    def wrap(*params,**kwargs):
+        call = wrap.callcount = wrap.callcount + 1
+
+        indent = ' ' * __report_indent[0]
+        fc = "%s(%s)" % (fn.__name__, ', '.join(
+            [a.__repr__() for a in params] +
+            ["%s = %s" % (a, repr(b)) for a,b in kwargs.items()]
+        ))
+
+        print "CALL: %s%s [#%s]" % (indent, fc, call)
+        __report_indent[0] += 1
+        ret = fn(*params,**kwargs)
+        __report_indent[0] -= 1
+        print "RETURN: %s%s %s [#%s]" % (indent, fc, repr(ret), call)
+
+        return ret
+    wrap.callcount = 0
+    return wrap    
