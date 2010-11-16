b33c6f73bd1ae95981b9771cdc2bcf9dfef6354e
Adam Harvey <aharvey@ccg.murdoch.edu.au>
2010-11-16 04:09:00 +0000

    Merge the aharvey-user-json-mixin branch.
    Squashed commit of the following:
    
    commit c8c38c43f6ab92c5122169e42b7d02284e76d156
    Author: aharvey <aharvey@f0de71f0-5f1c-0410-800e-c8b759593111>
    Date:   Tue Nov 16 04:05:39 2010 +0000
    
        Limit the querysets explicitly to superusers.
    
        git-svn-id: svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/branches/aharvey-user-json-mixin@10527 f0de71f0-5f1c-0410-800e-c8b759593111
    
    commit 39b33d7b9ac253fa41acfc2271a465eb411565f3
    Author: aharvey <aharvey@f0de71f0-5f1c-0410-800e-c8b759593111>
    Date:   Tue Nov 16 04:05:38 2010 +0000
    
        Add the JSON WS to the local models in FE.
    
        git-svn-id: svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/branches/aharvey-user-json-mixin@10526 f0de71f0-5f1c-0410-800e-c8b759593111
    
    commit 0882945095a3936e5330cad7f50b810ec799c29f
    Author: aharvey <aharvey@f0de71f0-5f1c-0410-800e-c8b759593111>
    Date:   Mon Nov 15 09:57:58 2010 +0000
    
        Implemented the same JSON mixin for django.contrib.auth.models.* as yabiadmin.
        Restricted to superusers only.
    
        git-svn-id: svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/branches/aharvey-user-json-mixin@10522 f0de71f0-5f1c-0410-800e-c8b759593111
    
    commit 6dc02f801dd69826188e692adaa98cde17fb19e6
    Author: aharvey <aharvey@f0de71f0-5f1c-0410-800e-c8b759593111>
    Date:   Mon Nov 15 09:50:44 2010 +0000
    
        Create branch aharvey-user-json-mixin
    
        git-svn-id: svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/branches/aharvey-user-json-mixin@10521 f0de71f0-5f1c-0410-800e-c8b759593111

--- yabi/yabife/trunk/yabife/admin.py
+++ yabi/yabife/trunk/yabife/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
+from django.contrib.webservices.ext import ExtJsonInterface
 from yabife.yabifeapp.admin import register
 
+import django.contrib.auth.admin
+import django.contrib.auth.models
+import django.contrib.sites.admin
+import django.contrib.sites.models
+
+
+# Overrides for the default django.contrib.auth ModelAdmin subclasses that
+# include the JSON mixin.
+class GroupAdmin(ExtJsonInterface, django.contrib.auth.admin.GroupAdmin):
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return django.contrib.auth.models.Group.objects.all()
+
+        return django.contrib.auth.models.Group.objects.none()
+
+class UserAdmin(ExtJsonInterface, django.contrib.auth.admin.UserAdmin):
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return django.contrib.auth.models.User.objects.all()
+
+        return django.contrib.auth.models.User.objects.none()
+
+
 site = admin.AdminSite(name="Yabi Frontend Administration")
+
+# Django Auth.
+site.register(django.contrib.auth.models.Group, GroupAdmin)
+site.register(django.contrib.auth.models.User, UserAdmin)
+
 register(site)
--- yabi/yabife/trunk/yabife/yabifeapp/admin.py
+++ yabi/yabife/trunk/yabife/yabifeapp/admin.py
@@ -1,12 +1,21 @@
 from django.contrib.admin import ModelAdmin
+from django.contrib.webservices.ext import ExtJsonInterface
 from yabife.yabifeapp.models import Appliance, User
 
 
-class ApplianceAdmin(ModelAdmin):
+class SuperuserOnlyModelAdmin(ModelAdmin):
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return self.model.objects.all()
+
+        return self.model.objects.none()
+
+
+class ApplianceAdmin(ExtJsonInterface, SuperuserOnlyModelAdmin):
     pass
 
  
-class UserAdmin(ModelAdmin):
+class UserAdmin(ExtJsonInterface, SuperuserOnlyModelAdmin):
     list_display = ("user", "appliance")
 
 
