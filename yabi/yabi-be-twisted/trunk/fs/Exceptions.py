cf9debe32c949cd6b6d16b6a6e0965e2a0fed424
Crispin Wellington <retrogradeorbit@gmail.com>
2010-11-17 04:06:47 +0000

    refactored exceptions

--- yabi/yabi-be-twisted/trunk/fs/Exceptions.py
+++ yabi/yabi-be-twisted/trunk/fs/Exceptions.py
@@ -1,12 +1,4 @@
+# -*- coding: utf-8 -*-
 """This file contains all the exceptions raised by problems encountered by the filesystem connectors
 """
-
-class PermissionDenied(Exception):
-    """Permission denied error"""
-    pass
-
-class InvalidPath(Exception):
-    """The path passed is an invalid path for this connector"""
-    pass
-
 
