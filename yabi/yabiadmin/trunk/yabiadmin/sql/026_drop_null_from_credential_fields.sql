a74392a650cd5a7f9acb5fbe73c7e442416797bc
Mark O'Shea <moshea@ccg.murdoch.edu.au>
2010-11-23 04:06:34 +0000

    correcting sql script

--- yabi/yabiadmin/trunk/yabiadmin/sql/026_drop_null_from_credential_fields.sql
+++ yabi/yabiadmin/trunk/yabiadmin/sql/026_drop_null_from_credential_fields.sql
@@ -2,10 +2,11 @@ BEGIN;
 UPDATE "yabi_credential" SET password = '' WHERE password is NULL;
 UPDATE "yabi_credential" SET cert = '' WHERE cert is NULL;
 UPDATE "yabi_credential" SET key = '' WHERE key is NULL;
+COMMIT;
 
+BEGIN;
 ALTER TABLE "yabi_credential" ALTER COLUMN "password" SET NOT NULL;
 ALTER TABLE "yabi_credential" ALTER COLUMN "cert" SET NOT NULL;
 ALTER TABLE "yabi_credential" ALTER COLUMN "key" SET NOT NULL;
-
 COMMIT;
 
