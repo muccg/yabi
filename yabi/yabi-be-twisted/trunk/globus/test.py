3a087085d0c1cefe783a29ee438a761925c1208b
Crispin Wellington <retrogradeorbit@gmail.com>
2011-01-05 05:17:25 +0000

    restructuring directories

--- yabi/yabi-be-twisted/trunk/globus/test.py
+++
@@ -1,7 +0,0 @@
-import CertificateProxy
-import GlobusURLCopy
-
-proxy=CertificateProxy.make_proxy()
-globus = GlobusURLCopy.GlobusURLCopy()
-
-print globus.WriteToRemote( proxy.ProxyFile("crispin"), "gsiftp://xe-gt4.ivec.org/scratch/bi01/cwellington/destination.txt" )
