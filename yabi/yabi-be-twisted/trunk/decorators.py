821b7e2a4677f048d318b6aad46f01093893d228
Crispin Wellington <retrogradeorbit@gmail.com>
2011-01-18 08:34:44 +0000

    lock and retry decorator first cuts

---
+++ yabi/yabi-be-twisted/trunk/decorators.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import stackless
+
+DEFAULT_FUNCTION_RETRY = 3
+
+def default_delay_generator():
+    delay = 10.0
+    while delay<60.0:
+        yield delay
+        delay *= 2.0
+    while True:
+        yield delay
+    
+def retry(num_retries = DEFAULT_FUNCTION_RETRY, delay = None):
+    def retry_decorator(f):
+        def new_func(*args, **kwargs):
+            num = num_retries
+            if delay:
+                gen = delay()
+            else:
+                gen = delay_generator()
+            while True:
+                try:
+                    return f(*args, **kwargs)               # exits on success
+                except Exception, E:
+                    if num:
+                        delay = gen.next()
+                        print "WARNING: retry-function",f,"raised exception",E,"... waiting",delay,"seconds and retrying",num,"more times..."
+                        num -= 1
+                    else:
+                        raise                               # out of retries... fail
+        return new_func
+    return retry_decorator
+
+def lock(maximum):
+    def lock_decorator(f):
+        if not hasattr(f,'_CONNECTION_COUNT'):
+            f._CONNECTION_COUNT = 0
+        def new_func(*args, **kwargs):
+            
+            # pre lock
+            while f._CONNECTION_COUNT >= maximum:
+                print "WARNING: max connection count reached for",f,"(%d)"%maximum
+                stackless.schedule()
+                
+            f._CONNECTION_COUNT += 1
+            
+            try:
+                return f(*args, **kwargs)
+            finally:
+                # post lock
+                f._CONNECTION_COUNT -= 1
+                
+        return new_func
+    return lock_decorator
+    
+    
