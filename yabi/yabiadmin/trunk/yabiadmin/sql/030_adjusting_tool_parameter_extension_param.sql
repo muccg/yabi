f2b6ba354ed6079c6f0cb7f5ecae26bd02d9e83a
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2011-02-16 07:13:42 +0000

    added commands needed to fix yabiengine.job entries for this release

--- yabi/yabiadmin/trunk/yabiadmin/sql/030_adjusting_tool_parameter_extension_param.sql
+++ yabi/yabiadmin/trunk/yabiadmin/sql/030_adjusting_tool_parameter_extension_param.sql
@@ -1,3 +1,14 @@
 ALTER TABLE yabi_toolparameter ADD COLUMN "extension_param_id" INTEGER REFERENCES "yabi_fileextension" ("id") DEFERRABLE INITIALLY DEFERRED;
 ALTER TABLE yabi_toolparameter DROP COLUMN "source_param_id";
 ALTER TABLE yabi_toolparameter ADD use_batch_filename boolean NOT NULL DEFAULT 'N';
+
+--- it may be neccessary to replace the commands in the running jobs on live after deployment of this patch and its release
+--- use this sql
+
+---begin;
+---update yabiengine_job set command = replace( command, '%', '__yabi_bp') where id in (select id from yabiengine_job where status = 'running');
+---end;
+
+---begin;
+---update yabiengine_job set command = replace( command, '$', '__yabi_fp') where id in (select id from yabiengine_job where status = 'running');
+---end;
