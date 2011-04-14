5b3e46453f80aaeb94e11e9cf3c5ffc3f560e4f7
Crispin Wellington <retrogradeorbit@gmail.com>
2011-04-14 06:41:05 +0000

    turned defaults to off

--- yabi/yabiadmin/trunk/yabiadmin/sql/032_add_copy_link_backend_flags.sql
+++ yabi/yabiadmin/trunk/yabiadmin/sql/032_add_copy_link_backend_flags.sql
@@ -1,8 +1,8 @@
 BEGIN;
-ALTER TABLE "yabi_backend" ADD COLUMN "lcopy_supported" boolean NOT NULL DEFAULT 'T';
-ALTER TABLE "yabi_backend" ADD COLUMN "link_supported" boolean NOT NULL DEFAULT 'T';
-ALTER TABLE "yabi_tool" ADD COLUMN "lcopy_supported" boolean NOT NULL DEFAULT 'T';
-ALTER TABLE "yabi_tool" ADD COLUMN "link_supported" boolean NOT NULL DEFAULT 'T';
+ALTER TABLE "yabi_backend" ADD COLUMN "lcopy_supported" boolean NOT NULL DEFAULT 'F';
+ALTER TABLE "yabi_backend" ADD COLUMN "link_supported" boolean NOT NULL DEFAULT 'F';
+ALTER TABLE "yabi_tool" ADD COLUMN "lcopy_supported" boolean NOT NULL DEFAULT 'F';
+ALTER TABLE "yabi_tool" ADD COLUMN "link_supported" boolean NOT NULL DEFAULT 'F';
 ALTER TABLE "yabiengine_stagein" ADD COLUMN "method" varchar(5) NOT NULL DEFAULT 'copy';
 ALTER TABLE "yabiengine_job" ADD COLUMN "preferred_stagein_method" varchar(5) NOT NULL DEFAULT 'copy';
 ALTER TABLE "yabiengine_job" ADD COLUMN "preferred_stageout_method" varchar(5) NOT NULL DEFAULT 'copy';
