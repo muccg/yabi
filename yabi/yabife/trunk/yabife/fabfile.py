af104c5f601d89f31e5032fa79a0658826a3f30b
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2011-07-21 16:18:05 +0800

    updated fab file for fe

--- yabi/yabife/trunk/yabife/fabfile.py
+++ yabi/yabife/trunk/yabife/fabfile.py
@@ -24,104 +24,57 @@
 # OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 # 
 ### END COPYRIGHT ###
-from fabric.api import env, local
+from fabric.api import env
 from ccgfab.base import *
-import os
 
-env.username = os.environ["USER"]
 env.app_root = '/usr/local/python/ccgapps/'
 env.app_name = 'yabife'
-env.app_install_names = ['yabife'] # use app_name or list of names for each install
-env.vc = 'svn'
-env.git_trunk_url = ""
-env.svn_trunk_url = "svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/trunk"
-env.svn_tags_url = "svn+ssh://store.localdomain/store/techsvn/ccg/yabi/yabife/tags"
+env.app_install_names = ['yabifeapp'] # use app_name or list of names for each install
+env.vc = 'mercurial'
 
 env.writeable_dirs.extend([]) # add directories you wish to have created and made writeable
 env.content_excludes.extend([]) # add quoted patterns here for extra rsync excludes
 env.content_includes.extend([]) # add quoted patterns here for extra rsync includes
+env.auto_confirm_purge = False #controls whether the confirmation prompt for purge is used
 
-class LocalPaths():
-
-    target = env.username
-
-    def getSettings(self):
-        return os.path.join(env.app_root, env.app_install_names[0], self.target, env.app_name, "settings.py")
-
-    def getProjectDir(self):
-        return os.path.join(env.app_root, env.app_install_names[0], self.target, env.app_name)
-
-    def getParentDir(self):
-        return os.path.join(env.app_root, env.app_install_names[0], self.target)
-
-    def getCeleryd(self):
-        return os.path.join(self.getProjectDir(), 'virtualpython/bin/celeryd')
-
-    def getVirtualPython(self):
-        return os.path.join(self.getProjectDir(), 'virtualpython/bin/python')
-
-
-localPaths = LocalPaths()
-
-
-def deploy():
+def deploy(auto_confirm_purge = False):
     """
     Make a user deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_deploy_user()
-    env.settings_file = env.app_root + env.app_name + "/" + os.environ["USER"] + "/" + env.app_name + "/settings.py"
-    print local("sed -i -r -e 's/<CCG_TARGET_NAME>/%s/g' %s"  % (os.environ["USER"], env.settings_file))
 
-def snapshot():
+def snapshot(auto_confirm_purge=False):
     """
     Make a snapshot deployment
     """
+    env.auto_confirm_purge=auto_confirm_purge
     _ccg_deploy_snapshot()
-    env.settings_file = env.app_root + env.app_name + "/snapshot/" + env.app_name + "/settings.py"
-    print local("sed -i -r -e 's/<CCG_TARGET_NAME>/%s/g' %s"  % ("snapshot", env.settings_file))
-    print local("sed -i -r -e 's/SENTRY_TESTING = False/SENTRY_TESTING = True/g' %s"  % env.settings_file)
-    print local("sed -i -r -e 's/LOGGING_LEVEL = logging.DEBUG/LOGGING_LEVEL = logging.WARNING/g' %s"  % env.settings_file)
 
-def release():
+def release(auto_confirm_purge=False):
     """
     Make a release deployment
     """
+    env.auto_confirm=auto_confirm_purge
     _ccg_deploy_release()
 
-def testrelease():
+def testrelease(auto_confirm_purge=False):
     """
     Make a release deployment using the dev settings file
     """
+    env.auto_confirm=auto_confirm_purge
     _ccg_deploy_release(devrelease=True)
 
-def purge():
+def purge(auto_confirm_purge=False):
     """
     Remove the user deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_purge_user()
 
-def purge_snapshot():
+def purge_snapshot(auto_confirm_purge = False):
     """
     Remove the snapshot deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_purge_snapshot()
-
-def syncdb():
-    """
-    syncdb using your deployment of yabi admin
-    """
-    manage("syncdb")
-
-def manage(opt="help"):
-    _django_env()
-    print local(localPaths.getVirtualPython() + " " + localPaths.getProjectDir() + "/manage.py " + opt, capture=False)
-
-
-def _django_env():
-    os.environ["DJANGO_SETTINGS_MODULE"]="settings"
-    os.environ["DJANGO_PROJECT_DIR"]=localPaths.getProjectDir()
-    os.environ["CELERY_LOADER"]="django"
-    os.environ["CELERY_CHDIR"]=localPaths.getProjectDir()
-    os.environ["PYTHONPATH"] = "/usr/local/etc/ccgapps/:" + localPaths.getProjectDir() + ":" + localPaths.getParentDir()
-    os.environ["PROJECT_DIRECTORY"] = localPaths.getProjectDir()
-
