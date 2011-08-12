d660cc85d036f2a67489f35096ee688e63e4fa00
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2011-08-12 17:33:52 +0800

    adding

---
+++ yabife/yabife/yabifeapp/utils.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+### BEGIN COPYRIGHT ###
+#
+# (C) Copyright 2011, Centre for Comparative Genomics, Murdoch University.
+# All rights reserved.
+#
+# This product includes software developed at the Centre for Comparative Genomics 
+# (http://ccg.murdoch.edu.au/).
+# 
+# TO THE EXTENT PERMITTED BY APPLICABLE LAWS, YABI IS PROVIDED TO YOU "AS IS," 
+# WITHOUT WARRANTY. THERE IS NO WARRANTY FOR YABI, EITHER EXPRESSED OR IMPLIED, 
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY RIGHTS. 
+# THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF YABI IS WITH YOU.  SHOULD 
+# YABI PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR
+# OR CORRECTION.
+# 
+# TO THE EXTENT PERMITTED BY APPLICABLE LAWS, OR AS OTHERWISE AGREED TO IN 
+# WRITING NO COPYRIGHT HOLDER IN YABI, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR 
+# REDISTRIBUTE YABI AS PERMITTED IN WRITING, BE LIABLE TO YOU FOR DAMAGES, INCLUDING 
+# ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE 
+# USE OR INABILITY TO USE YABI (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR 
+# DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES 
+# OR A FAILURE OF YABI TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER 
+# OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# 
+### END COPYRIGHT ###
+
+import memcache
+
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseNotFound, HttpResponseRedirect, HttpResponseServerError, HttpResponseUnauthorized
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import mail_admins
+
+from yaphc import Http, GetRequest, PostRequest, UnauthorizedError
+from yaphc.memcache_persister import MemcacheCookiePersister
+
+
+def memcache_client():
+    return memcache.Client(settings.MEMCACHE_SERVERS)
+
+
+def memcache_http(request):
+    user = request.user
+
+    mp = MemcacheCookiePersister(settings.MEMCACHE_SERVERS,
+            key='%s-cookies-%s' %(settings.MEMCACHE_KEYSPACE, request.session.session_key),
+            cache_time=settings.SESSION_COOKIE_AGE)
+          
+    yabiadmin = user.get_profile().appliance.url
+    return Http(base_url=yabiadmin, cache=False, cookie_persister=mp)
+
+
+def make_http_request(request, original_request, ajax_call):
+    try:
+        with memcache_http(original_request) as http:
+            try:
+                resp, contents = http.make_request(request)
+                our_resp = HttpResponse(contents, status=int(resp.status))
+                copy_non_empty_headers(resp, our_resp, ('content-disposition', 'content-type'))
+                return our_resp
+            except UnauthorizedError:
+                if ajax_call:
+                    return HttpResponseUnauthorized() 
+                else:
+                    return HttpResponseRedirect(settings.LOGIN_URL)
+    except ObjectDoesNotExist:
+        if ajax_call:
+            return HttpResponseUnauthorized() 
+        else:
+            return HttpResponseRedirect(settings.LOGIN_URL)
+
+
+def make_request_object(url, request):
+    params = {}
+    for k in request.REQUEST:
+        params[k] = request.REQUEST[k]
+    if request.method == 'GET':
+        return GetRequest(url, params)
+    elif request.method == 'POST':
+        files = [('file%d' % (i+1), f.name, f.temporary_file_path()) for i,f in enumerate(request.FILES.values())] 
+        return PostRequest(url, params, files=files)
+
+
+def copy_non_empty_headers(src, to, header_names):
+    for header_name in header_names:
+        header_value = src.get(header_name)
+        if header_value:
+            to[header_name] = header_value
+
+
+def mail_admins_no_profile(user):
+    mail_admins("User Profile Error", render_to_string("email/noprofile.txt", {
+        "user": user,
+    }))
+
+
+def preview_key(uri):
+    # The naive approach here is to use the file name encoded in such a way
+    # that memcache accepts it as a key, but that turns out to be problematic,
+    # as it's not uncommon for file names within YABI to be greater than the
+    # 250 character limit memcache imposes on key names. As a result, we'll
+    # hash the file name and accept the (extremely slight) risk of collisions.
+    file_hash = hashlib.sha512(uri).hexdigest()
+    return str("%s-preview-%s" % (settings.MEMCACHE_KEYSPACE, file_hash))
+
+
+def reencrypt_user_credentials(request, currentPassword, newPassword):
+    enc_request = PostRequest("ws/account/passchange", params={ "oldPassword": currentPassword, "newPassword": newPassword })
+    http = memcache_http(request)
+    resp, content = http.make_request(enc_request)
+    assert resp['status']=='200', (resp['status'], content)
