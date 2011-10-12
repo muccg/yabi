85bce7020e4d088a8a088592eea0e9b3209f2459
Crispin Wellington <retrogradeorbit@gmail.com>
2011-10-12 12:15:33 +0800

    make certificates optional if only ssl client is needed. Stay compulsory if SSL server is to be started

--- yabibe/yabibe/ServerContextFactory.py
+++ yabibe/yabibe/ServerContextFactory.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from conf import config
 config.read_config()
 config.sanitise()
@@ -14,6 +15,20 @@ class ServerContextFactory:
         This is a sample implementation that loads a certificate from a file
         called 'server.pem'."""
         ctx = SSL.Context(SSL.SSLv23_METHOD)
-        ctx.use_certificate_file(os.path.join(config.config['backend']['certfile']))
-        ctx.use_privatekey_file(os.path.join(config.config['backend']['keyfile']))
+        
+        #if we are not serving SSL, then the only SSL routines we are using will be HTTPS get and posts. So we don't NEED a certfile. But use it if its there
+        #if we are serving SSL, and these aren't set, blowup!
+        if config.config['backend']['start_https']:
+            assert os.path.exists(os.path.join(config.config['backend']['certfile']))
+            assert os.path.exists(os.path.join(config.config['backend']['keyfile']))
+            
+            ctx.use_certificate_file(os.path.join(config.config['backend']['certfile']))
+            ctx.use_privatekey_file(os.path.join(config.config['backend']['keyfile']))
+        else:
+            # otherwise optional
+            if os.path.exists(os.path.join(config.config['backend']['certfile'])):
+                ctx.use_certificate_file(os.path.join(config.config['backend']['certfile']))
+            if os.path.exists(os.path.join(config.config['backend']['keyfile'])):
+                ctx.use_privatekey_file(os.path.join(config.config['backend']['keyfile']))
+                
         return ctx
