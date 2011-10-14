3b0513008f481b4639286c655d1c7dc588915ed1
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2011-10-14 16:07:26 +0800

    job also uses max_memory column

--- yabiadmin/yabiadmin/sql/045_change_max_memory_to_varchar.sql
+++ yabiadmin/yabiadmin/sql/045_change_max_memory_to_varchar.sql
@@ -3,4 +3,12 @@ ALTER TABLE yabi_tool ADD COLUMN new_max_memory VARCHAR(64);
 UPDATE yabi_tool SET new_max_memory = max_memory::varchar WHERE max_memory IS NOT NULL;
 ALTER TABLE yabi_tool DROP COLUMN max_memory;
 ALTER TABLE yabi_tool RENAME COLUMN new_max_memory TO max_memory;
-commit;
+COMMIT;
+
+
+BEGIN;
+ALTER TABLE yabiengine_job ADD COLUMN new_max_memory VARCHAR(64);
+UPDATE yabiengine_job SET new_max_memory = max_memory::varchar WHERE max_memory IS NOT NULL;
+ALTER TABLE yabiengine_job DROP COLUMN max_memory;
+ALTER TABLE yabiengine_job RENAME COLUMN new_max_memory TO max_memory;
+COMMIT;
