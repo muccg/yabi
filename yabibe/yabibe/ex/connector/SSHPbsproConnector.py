e4bcd32721299240375034a8a8dae8ca64f7d6b0
Crispin Wellington <retrogradeorbit@gmail.com>
2011-12-20 14:29:38 +0800

    fixed more credtype problem. this time with pbspro

--- yabibe/yabibe/ex/connector/SSHPbsproConnector.py
+++ yabibe/yabibe/ex/connector/SSHPbsproConnector.py
@@ -152,7 +152,7 @@ class SSHPbsproConnector(ExecConnector, ssh.KeyStore.KeyStore):
         
         
         if not creds:
-            creds = sshauth.AuthProxyUser(yabiusername, SCHEMA, username, host, "/")
+            creds = sshauth.AuthProxyUser(yabiusername, SCHEMA, username, host, "/", credtype="exec")
     
         usercert = self.save_identity(creds['key'])
         
