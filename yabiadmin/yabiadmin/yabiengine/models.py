1d7818f4fba099cf2cfc35ae80105f02301f9a3e
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2012-02-02 15:33:47 +0800

    Fixes bug that allowed a Job marked completed when some of the tasks were still executing.

--- yabiadmin/yabiadmin/yabiengine/models.py
+++ yabiadmin/yabiadmin/yabiengine/models.py
@@ -176,10 +176,12 @@ class Job(models.Model, Editable, Status):
         Checks all the tasks for a job and sets the job status based on precedence of the task status.
         The order of the list being checked is therefore important.
         '''
-        for status in [STATUS_ERROR, 'stagein', 'mkdir', 'exec', 'exec:active', 'exec:pending', 'exec:unsubmitted', 'stageout', 'cleaning', STATUS_BLOCKED, STATUS_READY, STATUS_COMPLETE]:
+        for status in [STATUS_ERROR, 'exec:error', 'pending', 'requested', 'stagein', 'mkdir', 'exec', 'exec:active', 'exec:pending', 'exec:unsubmitted', 'exec:running', 'exec:cleanup', 'exec:done', 'stageout', 'cleaning', STATUS_BLOCKED, STATUS_READY, STATUS_COMPLETE]:
             if Task.objects.filter(job=self, status=status):
                 # we need to map the task status values to valid job status values
-                if status.startswith('exec') or status in ['stageout', 'cleaning', 'stagein', 'mkdir']:
+                if status == 'exec:error':
+                    self.status = STATUS_ERROR
+                elif status.startswith('exec') or status in ['stageout', 'cleaning', 'stagein', 'mkdir']:
                     self.status = STATUS_RUNNING
                 else:
                     self.status = status
