afeb59088ffb14a78d791db819a2c7c10dc97223
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2012-02-17 14:15:04 +0800

    Adds yabitests/README Separates the existing tests in different files.

--- yabitests/simple_tool_test.py
+++ yabitests/simple_tool_test.py
@@ -1,27 +1,4 @@
-import unittest
 from support import YabiTestCase
-
-class FirstTest(unittest.TestCase):
-    def test_success(self):
-        self.assertTrue(True)
-
-class NotLoggedInTest(YabiTestCase):
-
-    def test_run_yabish_no_args(self):
-        result = self.yabi.run()
-        self.assertTrue('usage:' in result.stdout)
-
-    def test_unsuccessful_login(self):
-        self.assertTrue(not self.yabi.login('tszabo', 'INVALID_PASS'))
-
-    def test_successful_login(self):
-        self.assertTrue(self.yabi.login())
-
-
-class ToolNotSetupTest(YabiTestCase):
-    def test_hostname_not_setup(self):
-        result = self.yabi.run('hostname')
-        self.assertTrue('Unknown tool name "hostname"' in result.stderr)
 
 class HostnameTest(YabiTestCase):
     @classmethod
