94b879a71621a4ca728e3374fe7eefa598c69d2e
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2012-03-30 11:55:27 +0800

    Changes Credential tests to not rely on the existence of the demo user.

--- yabiadmin/yabiadmin/yabi/tests.py
+++ yabiadmin/yabiadmin/yabi/tests.py
@@ -3,6 +3,7 @@
 from django.utils import unittest as unittest
 from django.test.client import Client
 
+from django.contrib.auth.models import User as DjangoUser
 from yabiadmin.yabi.models import Credential, User
 
 class StatusPageTest(unittest.TestCase):
@@ -14,56 +15,47 @@ class StatusPageTest(unittest.TestCase):
 
 class CredentialTests(unittest.TestCase):
     def setUp(self):
-        self.credential = Credential.objects.get(description = 'null credential', user__name='demo')
-
+        self.user = User.objects.create(name=u'győzike')
+        self.django_user = DjangoUser.objects.create(username=u'győzike')
+        self.django_user.set_password('pass')
+        self.django_user.save()
+        self.credential = Credential.objects.create(description='null credential', username=self.user.name, user=self.user)
+       
     def tearDown(self):
         self.credential.clear_cache()
-        
+        self.credential.delete()
+        self.user.delete()
+        self.django_user.delete()
+
+    def test_cache_keyname_replaces_unicode_character(self):
+        self.assertTrue('\xc5\x91' in self.credential.cache_keyname())
+
     def test_not_cached(self):
         self.assertFalse(self.credential.is_cached)
 
     def test_cache(self):
         self.credential.send_to_cache()
         self.assertTrue(self.credential.is_cached)
+        self.assertEqual(self.credential.get_from_cache(), None)
 
     def test_get_from_cache(self):
-        self.credential.password = 'pass1'
+        self.credential.password = 'pass'
         self.credential.send_to_cache()
         self.credential.password = 'pass2'
         self.credential.get_from_cache()
-        self.assertEqual(self.credential.password, 'pass1')
+        self.assertEqual(self.credential.password, 'pass', 'get_from_cache() should set the password back to original')
 
     def test_clear_from_cache(self):
         self.credential.send_to_cache()
         self.credential.clear_cache()
         self.assertFalse(self.credential.is_cached)
 
-class CredentialUnicodUsernameTests(unittest.TestCase):
-    def setUp(self):
-        self.user = User.objects.create(name=u'győzike')
-        self.credential = Credential.objects.create(description='null credential', username=self.user.name, user=self.user)
-       
-    def test_cache_keyname_replaces_unicode_character(self):
-        self.assertTrue('\xc5\x91' in self.credential.cache_keyname())
-
-    def test_cache(self):
-        self.credential.send_to_cache()
-        self.assertTrue(self.credential.is_cached)
-        self.assertEqual(self.credential.get_from_cache(), None)
-
-    def tearDown(self):
-        self.credential.clear_cache()
-        self.credential.delete()
-        self.user.delete()
-
-class LoginCredentialTest(unittest.TestCase):
     def test_login_decrypts_credential(self):
         client = Client()
-        credential = Credential.objects.get(description='null credential', user__name='demo')
         response = client.post('/ws/login/', 
-                {'username': 'demo', 'password': 'demo'})
+                {'username': self.django_user.username, 'password': 'pass'})
         self.assertEqual(response.status_code, 200)
         self.assertTrue("All credentials were successfully decrypted" in response.content)
-        self.assertTrue(credential.is_cached)
+        self.assertTrue(self.credential.is_cached)
 
 
