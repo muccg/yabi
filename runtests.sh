741bbd3e8a60a0cb657cc06a0326a69087909bdb
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2012-12-15 17:38:50 +0800

    adding support for dev_mysql, dev_postgres and quickstart

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 source virt_yabiadmin/bin/activate
-export DJANGO_SETTINGS_MODULE="yabiadmin.settings"
 
-export YABI_CONFIG="dev_mysql"
-export YABI_CONFIG="dev_postgres"
-export YABI_CONFIG="quickstart"
+if [ $YABI_CONFIG = "" ]; then
+    YABI_CONFIG="dev_mysql"
+fi
+
+case $YABI_CONFIG in
+dev_mysql)
+    export PYTHONPATH=yabiadmin
+    export DJANGO_SETTINGS_MODULE="yabiadmin.settings"
+    ;;
+dev_postgres)
+    export PYTHONPATH=yabiadmin
+    export DJANGO_SETTINGS_MODULE="yabiadmin.postgresqlsettings"
+    ;;
+quickstart)
+    export DJANGO_SETTINGS_MODULE="yabiadmin.quickstartsettings"
+    ;;
+*)
+    echo "No YABI_CONFIG set, exiting"
+    exit 1
+esac
+
+python -c "from django.db import models"
+python -c "import $DJANGO_SETTINGS_MODULE"
+echo "DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE"
 
 nosetests $@
