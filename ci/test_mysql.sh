6828b0d3605e1dc4a7dd570faa479a638b443ed7
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2013-04-30 00:08:39 +0800

    ci debug

--- ci/test_mysql.sh
+++ ci/test_mysql.sh
@@ -3,6 +3,8 @@ set -ex
 echo $BASH_SOURCE
 echo $-
 ./yabictl.sh install
+echo $?
 mysql -v -uroot -e "drop database test_yabi;" || true
 mysql -v -uroot -e "create database test_yabi default charset=UTF8;" || true
 ./yabictl.sh test_mysql
+echo $?
