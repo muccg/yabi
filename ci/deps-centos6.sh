0bab2e9c5c11fc1408fbcc8c2e6b1f0cf749fcf0
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2013-05-11 17:17:07 +0800

    lock ci dep for mysql

--- ci/deps-centos6.sh
+++ ci/deps-centos6.sh
@@ -2,6 +2,6 @@ set -ex
 echo $BASH_SOURCE
 echo $-
 sudo yum install -y libevent-devel
-sudo yum install -y mysql-server
+sudo yum install -y mysql-server-5.1.69-1.el6_4.x86_64
 sudo yum install -y postgresql-devel
 sudo service mysqld restart
