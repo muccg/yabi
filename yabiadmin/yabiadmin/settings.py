4d854caeca7113ffee43ef84772094222699d8b6
Lee Render <lrender@ccg.murdoch.edu.au>
2013-06-28 13:25:40 +0800

    YABI-248  removal of mako loader

--- yabiadmin/yabiadmin/settings.py
+++ yabiadmin/yabiadmin/settings.py
@@ -166,8 +166,8 @@ TEMPLATE_DEBUG = DEBUG
 # see: https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
 TEMPLATE_LOADERS = [
     'django.template.loaders.app_directories.Loader',
-    'ccg.template.loaders.makoloader.filesystem.Loader',
-    #'django.template.loaders.filesystem.Loader'
+    #'ccg.template.loaders.makoloader.filesystem.Loader',
+    'django.template.loaders.filesystem.Loader'
 ]
 
 # see: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
