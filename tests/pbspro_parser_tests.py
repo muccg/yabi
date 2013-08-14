c2c78a1771d8e4d706e5e9c2fc04c90423668e05
Lee Render <lrender@ccg.murdoch.edu.au>
2013-08-14 14:01:45 +0800

    YABI-330  test for  submission error status

--- tests/pbspro_parser_tests.py
+++ tests/pbspro_parser_tests.py
@@ -6,9 +6,16 @@ class QSubParseTestCase(unittest.TestCase):
     def setUp(self):
         self.parser = PBSProParser()
         self.good_lines = ["1223.epic\n", "dontcare\n", "dontcare more\n"]
+        self.missing_script_lines_error_lines = ["qsub: script file:: No such file or directory\n"]
 
     def test_qsub_parser_picks_up_remote_id(self):
         result = self.parser.parse_qsub(self.good_lines, [])
         self.assertTrue(result.remote_id == "1223", "PBSProParser failed to parse qsub remote id: Expected: 1223 Actual: %s" % result.remote_id)
         self.assertTrue(result.status == PBSProQSubResult.JOB_SUBMITTED, "PBSProParser qsub result has incorrect status: Expected: %s Actual %s" % (PBSProQSubResult.JOB_SUBMITTED, result.status))
+
+    def test_qsub_parser_returns_error_status_when_no_such_script(self):
+        result = self.parser.parse_qsub([],self.missing_script_lines_error_lines)
+        self.assertTrue(result.remote_id is None,"PBSProParser qsub test for missing script - should not assign remote id: Expected: None Actual: %s" % result.remote_id)
+        self.assertTrue(result.status == PBSProQSubResult.JOB_SUBMISSION_ERROR,"PBSProParser qsub for missing script. Expected: %s Actual: %s" % (PBSProQSubResult.JOB_SUBMISSION_ERROR, result.status))
+
 
