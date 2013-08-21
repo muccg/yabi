95d427bb0912721cbd16ede7a399f538ea1f3118
Lee Render <lrender@ccg.murdoch.edu.au>
2013-08-21 11:45:10 +0800

    YABI-337 added response methods to parse result states. Changed qsub base template to include stderr and stdout files.

--- yabiadmin/yabiadmin/backend/schedulerexecbackend.py
+++ yabiadmin/yabiadmin/backend/schedulerexecbackend.py
@@ -16,7 +16,7 @@ class SchedulerExecBackend(SSHBackend):
                     "cat<<EOS>$script_temp_file_name",
                     "{1}",
                     "EOS",
-                    "qsub $script_temp_file_name"])
+                    "qsub -o '{2}' -e '{3}' $script_temp_file_name"])
 
     QSTAT_TEMPLATE = "\n".join(["#!/bin/sh",
                                 "qstat -f -1 {0}"])
@@ -24,31 +24,50 @@ class SchedulerExecBackend(SSHBackend):
     def __init__(self, *args, **kwargs):
         super(SchedulerExecBackend, self).__init__(*args, **kwargs)
         self.parser = None
+        self.submission_script_name = None
+        self.submission_script_body = None
+        self.qsub_script_body = None
+        self.stdout_file = None
+        self.stderr_file = None
 
     def submit_task(self):
         qsub_result = self._run_qsub()
         if qsub_result.status == qsub_result.JOB_SUBMITTED:
-            self.task.remote_id = qsub_result.remote_id
-            self.task.save()
-            logger.debug("Yabi Task {0} submitted to {1} OK. remote id = {2}".format(self.task.pk,
+            self._job_submitted_response(qsub_result)
+        else:
+            self._job_not_submitted_response(qsub_result)
+
+
+    def _job_submitted_response(self, qsub_result):
+        self.task.remote_id = qsub_result.remote_id
+        self.task.save()
+        logger.debug("Yabi Task {0} submitted to {1} OK. remote id = {2}".format(self.task.pk,
                                                                                      self.SCHEDULER_NAME,
                                                                                      self.task.remote_id))
-        else:
-            raise Exception("Error submitting remote job to {0} for yabi task {1} {2}".format(self.SCHEDULER_NAME,
-                                                                                              self.task.pk,
-                                                                                              qsub_result.status))
+
+    def _job_not_submitted_response(self, qsub_result):
+        raise Exception("Error submitting remote job to {0} for yabi task {1} {2}".format(self.SCHEDULER_NAME,
+                                                                                          self.task.pk,
+                                                                                          qsub_result.status))
 
     def _run_qsub(self):
         exec_scheme, exec_parts = uriparse(self.task.job.exec_backend)
         working_scheme, working_parts = uriparse(self.working_output_dir_uri())
-        submission_script_name = self._generate_remote_script_name()
-        logger.debug("creating qsub script %s" % submission_script_name)
-        submission_script_body = self.get_submission_script(exec_parts.hostname, working_parts.path)
-        qsub_script_body = self.QSUB_TEMPLATE.format(submission_script_name, submission_script_body)
-        logger.debug("qsub script:\n%s" % qsub_script_body)
-        stdout, stderr = self._exec_script(qsub_script_body)
+        self.submission_script_name = self._generate_remote_script_name()
+        logger.debug("creating qsub script %s" % self.submission_script_name)
+        self.submission_script_body = self.get_submission_script(exec_parts.hostname, working_parts.path)
+        self.stdout_file = os.path.join(working_parts.path, "STDOUT.txt")
+        self.stderr_file = os.path.join(working_parts.path, "STDERR.txt")
+        self.qsub_script_body = self._get_qsub_body()
+        logger.debug("qsub script:\n%s" % self.qsub_script_body)
+        stdout, stderr = self._exec_script(self.qsub_script_body)
         qsub_result = self.parser.parse_qsub(stdout, stderr)
         return qsub_result
+
+    def _get_qsub_body(self):
+        return self.QSUB_TEMPLATE.format(
+            self.submission_script_name, self.submission_script_body,
+            self.stdout_file, self.stderr_file)
 
     def _get_polling_script(self):
         return self.QSTAT_TEMPLATE.format(self.task.remote_id)
@@ -59,28 +78,30 @@ class SchedulerExecBackend(SSHBackend):
         qstat_result = self.parser.parse_qstat(self.task.remote_id, stdout, stderr)
         return qstat_result
 
+    def _job_running_response(self, qstat_result):
+        logger.debug("remote job %s for yabi task %s is stilling running" % (self.task.remote_id, self.task))
+        retry_ex = RetryException("remote job %s still running" % self.task.remote_id)
+        retry_ex.backoff_strategy = RetryException.BACKOFF_STRATEGY_CONSTANT
+        raise retry_ex
+
+    def _job_not_found_response(self, qstat_result):
+        # NB. for psbpro and torque this is an error, for other subclasses it isn't
+        raise NotImplementedError()
+
+    def _job_completed_response(self, qstat_result):
+        logger.debug("yabi task %s succeeded" % self.task.pk)
+
+    def _unknown_job_status_response(self, qstat_result):
+        raise Exception("Yabi task %s unknown state: %s" % (self.task.pk, qstat_result.status))
+
     def poll_task_status(self):
         qstat_result = self._run_qstat()
-
         if qstat_result.status == qstat_result.JOB_RUNNING:
-            logger.debug("remote job %s for yabi task %s is stilling running" % (self.task.remote_id, self.task))
-            # cause retry of polling task ( not sure if this is the best
-            retry_ex = RetryException("remote job %s still running" % self.task.remote_id)
-            retry_ex.backoff_strategy = RetryException.BACKOFF_STRATEGY_CONSTANT
-            raise retry_ex
-
+            self._job_running_response(qstat_result)
         elif qstat_result.status == qstat_result.JOB_NOT_FOUND:
             logger.debug("qstat for remote job %s did not produce results" % self.task.remote_id)
-            # For Torque this is an error
-            raise Exception("Remote job %s for Yabi task %s not found by qstat" % (self.task.remote_id, self.task.pk))
-
-        elif qstat_result.status == qstat_result.JOB_SUCCEEDED:
-            logger.debug("yabi task %s succeeded" % self.task.pk)
-
-        elif qstat_result.status == qstat_result.JOB_FAILED:
-            logger.debug("remote job for yabi task %s failed" % self.task.pk)
-            # how to cause a job resubmission?
-            raise Exception("Yabi task %s failed remotely" % self.task.pk)
+            self._job_not_found_response(qstat_result)
+        elif qstat_result.status == qstat_result.JOB_COMPLETED:
+            self._job_completed_response(qstat_result)
         else:
-            raise Exception("Yabi task %s unknown state: %s" % (self.task.pk, qstat_result.status))
-
+            self._unknown_job_status_response(qstat_result)
