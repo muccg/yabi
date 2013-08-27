5a935f39fb9edd4a2b7a495cc30066ca6c81dc06
Lee Render <lrender@ccg.murdoch.edu.au>
2013-08-27 14:39:44 +0800

    Back to using erlang solutuons repo

--- ci/deps-centos6.sh
+++ ci/deps-centos6.sh
@@ -2,7 +2,6 @@ set -ex
 echo $BASH_SOURCE
 echo $-
 sudo rpm --import http://binaries.erlang-solutions.com/debian/erlang_solutions.asc
-sudo wget -O /etc/yum.repos.d/epel-erlang.repo http://repos.fedorapeople.org/repos/peter/erlang/epel-erlang.repo
 echo installing erlang
 sudo yum install erlang
 wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.3/rabbitmq-server-3.1.3-1.noarch.rpm
