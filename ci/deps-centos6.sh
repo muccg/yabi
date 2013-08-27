9267521e2e13878aaf94894ff13e0c2021ef9922
Lee Render <lrender@ccg.murdoch.edu.au>
2013-08-27 10:40:41 +0800

    fix deps for test environment

--- ci/deps-centos6.sh
+++ ci/deps-centos6.sh
@@ -1,7 +1,6 @@
 set -ex
 echo $BASH_SOURCE
 echo $-
-sudo yum install -y libevent-devel
 sudo yum install -y mysql-server-5.1.69-1.el6_4.x86_64
-sudo yum install -y postgresql-devel
+sudo yum install -y postgresql-devel libxslt-devel libxml2-devel
 sudo service mysqld restart
