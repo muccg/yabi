a5f880c6123b60ffc080e198f96e9ecf4b57d48b
Lee Render <lrender@ccg.murdoch.edu.au>
2013-08-29 09:39:37 +0800

    removed import of psutil no longer used

--- yabiadmin/yabiadmin/yabi/adminviews.py
+++ yabiadmin/yabiadmin/yabi/adminviews.py
@@ -27,7 +27,6 @@
 ### END COPYRIGHT ###
 # -*- coding: utf-8 -*-
 import sys
-import psutil
 from urllib import quote
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
