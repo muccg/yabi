51631efbabb12b422df9617b4ef569d8d2833ce4
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2013-09-26 10:24:14 +0800

    Bump version to 7.1.0, django upgrade has settings changes that warrant bumping above 7.0.1

--- yabiadmin/setup.py
+++ yabiadmin/setup.py
@@ -64,7 +64,7 @@ if not importlib_available:
     install_requires.append('importlib==1.0.1')
 
 setup(name='yabiadmin',
-    version='7.0.1',
+    version='7.1.0',
     description='Yabi Admin',
     long_description='Yabi front end and administration web interface',
     author='Centre for Comparative Genomics',
