545439fd7d502b41b5a836a0e3949987075ae430
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2013-10-08 17:13:09 +0800

    YABI-379: Forgot to add the sshparsers.py in the previous commit.

---
+++ yabiadmin/yabiadmin/backend/sshparsers.py
@@ -0,0 +1,63 @@
+import re
+import logging
+import string
+logger = logging.getLogger(__name__)
+
+
+class SSHSubmitResult(object):
+    JOB_SUBMITTED = "JOB SUBMITTED"
+    JOB_SUBMISSION_ERROR = "JOB SUBMISSION ERROR"
+
+    def __init__(self):
+        self.remote_id = None
+        self.status = None
+        self.error = None
+
+class SSHPsResult(object):
+    JOB_RUNNING = "JOB RUNNING"
+    JOB_NOT_FOUND = "JOB NOT FOUND BY PS"
+    JOB_COMPLETED = "JOB COMPLETED"
+
+    def __init__(self):
+        self.remote_id = None
+        self.status = None
+
+
+
+class SSHParser(object):
+    SUBMISSION_OUTPUT = "^(?P<remote_id>\d+)"
+
+    def parse_qsub(self, stdout, stderr):
+        result = SSHSubmitResult()
+
+        if len(stderr) > 0:
+
+            result.status = SSHSubmitResult.JOB_SUBMISSION_ERROR
+            result.error = "\n".join(stderr)
+            logger.debug('Got something in stderr:\n%s', result.error)
+            return result
+
+        for line in stdout:
+            s = line.strip()
+            m = re.match(self.SUBMISSION_OUTPUT, s)
+            if m:
+                result.remote_id = m.group("remote_id")
+                result.status = SSHSubmitResult.JOB_SUBMITTED
+                return result
+
+        logger.debug("Didn't match stdout:\n%s", stdout)
+
+        result.status = SSHSubmitResult.JOB_SUBMISSION_ERROR
+        return result
+
+    def parse_qstat(self, remote_id, stdout, stderr):
+        result = SSHPsResult()
+        result.remote_id = remote_id
+        for line in [s.strip() for s in stdout if s.strip() != '']:
+            if remote_id == line:
+                result.status = SSHPsResult.JOB_RUNNING
+                return result
+
+        result.status = SSHPsResult.JOB_COMPLETED
+        return result
+
