ec945f5a32c43f31f2d1c996919136fcbec12898
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2013-10-11 09:10:18 +0800

    YABI-304: Implemented abort_task on SSHBackend.

--- yabiadmin/yabiadmin/backend/sshbackend.py
+++ yabiadmin/yabiadmin/backend/sshbackend.py
@@ -38,8 +38,11 @@ import traceback
 import paramiko
 import tempfile
 import logging
+import time
 logger = logging.getLogger(__name__)
 
+
+WAIT_TO_TERMINATE_SECS = 3
 
 class SSHBackend(SchedulerExecBackend):
     SCHEDULER_NAME = "SSH"
@@ -60,10 +63,31 @@ echo "$!"
 ps -o pid= -p {0}
 """
 
+    KILL_COMMAND_TEMPLATE = """
+#!/bin/sh
+kill {1} {0}
+"""
+
+
     def __init__(self, *args, **kwargs):
         super(SSHBackend, self).__init__(*args, **kwargs)
         self.parser = SSHParser()
 
+    # TODO this looks very similar to LocalExecBackend#abort_task()
+    # Maybe, refactor later.
+    def abort_task(self):
+        pid = self.task.remote_id
+        if not self.is_process_running():
+            logger.info("Couldn't kill process of task %s. Process with id %s isn't running", self.task.pk, pid)
+            return
+
+        self.kill_process(pid)
+        time.sleep(WAIT_TO_TERMINATE_SECS)
+        if not self.is_process_running():
+            return
+
+        logger.info("Process %s (task %s) not terminated on SIGTERM. Sending SIGKILL", pid, self.task.pk)
+        self.kill_process(pid, with_SIGKILL=True)
 
     def _get_submission_wrapper_script(self):
         return self.RUN_COMMAND_TEMPLATE.format(
@@ -73,6 +97,19 @@ ps -o pid= -p {0}
     def _get_polling_script(self):
         return self.PS_COMMAND_TEMPLATE.format( self.task.remote_id)
         
+    def is_process_running(self):
+        result = self._poll_job_status()
+        return result.status == result.JOB_RUNNING
+
+    def kill_process(self, pid, with_SIGKILL=False):
+        kill_script = self._get_kill_script(with_SIGKILL)
+        stdout, stderr = self.executer.exec_script(kill_script)
+        if stderr:
+            log.error("Couldn't kill process %s. STDERR:\n%s", pid, stderr)
+     
+    def _get_kill_script(self, with_SIGKILL):
+        signal = "-KILL" if with_SIGKILL else ""
+        return self.KILL_COMMAND_TEMPLATE.format(self.task.remote_id, signal)
 
     def local_copy(self,src,dest, recursive=False):
         script = """
