799ad0d746984d7383bf45d19afb09b8827f6b78
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2013-12-05 16:01:12 +0800

    Remove ws_backend_views.

--- yabiadmin/yabiadmin/yabi/wsurls.py
+++ yabiadmin/yabiadmin/yabi/wsurls.py
@@ -59,21 +59,6 @@ urlpatterns += patterns(
     url(r'^modify_backend/name/(?P<scheme>[a-zA-Z_]*[a-zA-Z0-9_\-\.]*)/(?P<hostname>[a-zA-Z_]*[a-zA-Z0-9_\-\.]*)[/]*$', 'modify_backend_by_name'),
 )
 
-# backend webservices
-# TODO this is the only backend webservice and now needs to be non-SSL where
-# all the frontend ws urls are SSL. We should move this.
-urlpatterns += patterns(
-    'yabiadmin.yabi.ws_backend_views',
-    url(r'^credential/fs/(?P<yabiusername>[a-zA-Z_][a-zA-Z0-9_\-\.]*)[/]*$', 'fs_credential_uri', {'SSL': True}, name='credential_uri'),
-    url(r'^credential/exec/(?P<yabiusername>[a-zA-Z_][a-zA-Z0-9_\-\.]*)[/]*$', 'exec_credential_uri', {'SSL': True}, name='credential_uri'),
-    url(r'^backend/(?P<scheme>\w+)/(?P<hostname>[\w\.0-9\-]+)[/]*$', 'backend_connection_limit', name='backend_connection_limit'),
-    url(r'^hostkeys[/]*$', 'get_hostkeys', name='get_hostkeys'),
-    url(r'^hostkey/deny[/]*$', 'report_denied_hostkey', name='report_denied_hostkey'),
-    #url(r'^backend/(?P<scheme>\w+)/(?P<hostname>\w+)/(?P<port>\w+)/(?P<path>\w+)', 'backend_connection_limit', {'SSL':False}, name='backend_connection_limit'),
-    #url(r'^credential_deprecated/(?P<yabiusername>\w+)/(?P<scheme>\w+)/(?P<username>\w+)/(?P<hostname>[\w\-.]+)/(?P<detail>\w+)[/]*$', 'credential_detail', name='credential_detail'),
-    #url(r'^credential/(?P<yabiusername>\w+)/(?P<scheme>\w+)/(?P<username>\w+)/(?P<hostname>[\w\-.]+)[/]*$', 'credential', name='credential'),
-)
-
 # yabish webservices
 urlpatterns += patterns(
     'yabiadmin.yabi.ws_yabish_views',
