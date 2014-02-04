d9484c98fd140aab2f08c7aebe3b8dda0cb026a9
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2014-02-04 11:10:46 +0800

    Cleaning up command line template SwitchFilename tests.

--- yabiadmin/yabiadmin/yabiengine/tests.py
+++ yabiadmin/yabiadmin/yabiengine/tests.py
@@ -17,18 +17,34 @@ import hmac
 from django.conf import settings
 
 
-class CommandLineTemplateTest(unittest.TestCase):
+class SwitchFilenameWithExtensionTest(unittest.TestCase):
 
-    def test_switch_filename(self):
-        s = SwitchFilename(template=make_fname, extension='bls')
-        s.set('test.txt')
-        self.assertEquals('"test.bls"', '%s' % s)
-        s.set('test.bls')
-        self.assertEquals('"test.bls"', '%s' % s)
-        s.set('test.fa.txt')
-        self.assertEquals('"test.fa.bls"', '%s' % s)
-        s = SwitchFilename(template=make_fname)
-        s.set('test.txt')
-        self.assertEquals('"test.txt"', '%s' % s)
-        s.set('test')
-        self.assertEquals('"test"', '%s' % s)
+    def setUp(self):
+        self.switch = SwitchFilename(template=make_fname, extension='bls')
+
+    def test_when_no_extension_adds_extension(self):
+        self.switch.set('test')
+        self.assertEquals('"test.bls"', '%s' % self.switch)
+
+    def test_when_one_extension_changes_extension(self):
+        self.switch.set('test.txt')
+        self.assertEquals('"test.bls"', '%s' % self.switch)
+
+    def test_when_2_extensions_changes_last_extension(self):
+        self.switch.set('test.fa.txt')
+        self.assertEquals('"test.fa.bls"', '%s' % self.switch)
+
+
+class SwitchFilenameWithoutExtensionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.switch = SwitchFilename(template=make_fname)
+
+    def test_when_no_extension_doesnot_add_extension(self):
+        self.switch.set('test')
+        self.assertEquals('"test"', '%s' % self.switch)
+
+    def test_doesnot_change_extension(self):
+        self.switch.set('test.txt')
+        self.assertEquals('"test.txt"', '%s' % self.switch)
+
