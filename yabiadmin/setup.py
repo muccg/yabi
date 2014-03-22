4aa66d415cc07e7cb2a9c4a861f382e0e835fc56
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-03-22 17:08:07 +0800

    Bump version to 7.2.0

--- yabiadmin/setup.py
+++ yabiadmin/setup.py
@@ -103,7 +103,7 @@ if not importlib_available:
     install_requires.append('importlib>=1.0.1,<1.1.0')
 
 setup(name='yabiadmin',
-      version='7.1.11',
+      version='7.2.0',
       description='Yabi Admin',
       long_description='Yabi front end and administration web interface',
       author='Centre for Comparative Genomics',
