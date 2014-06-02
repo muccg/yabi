2b670f338d35c476879ccd6c5b03026d7e56fe91
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-06-02 15:38:14 +0800

    Update rpmbuild.pp

--- puppet/rpmbuild.pp
+++ puppet/rpmbuild.pp
@@ -1,14 +1,4 @@
 # A centos instance for building RPM packages
 node default {
-  include ccgcommon
-  include ccgcommon::source
-  include python
-  include repo::epel
-  include repo::ius
-  include repo::pgrpms
-
-  file { '/usr/local/src':
-    ensure => directory,
-    mode   => '0777',
-  }
+  include role::rpmbuild
 }
