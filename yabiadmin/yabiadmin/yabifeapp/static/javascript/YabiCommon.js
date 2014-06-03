db6a8e6c8bd2e1253b32823f30b6ff781d447da1
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2014-06-03 14:18:45 +0800

    YABI-505: File extension support for a set of characters

--- yabiadmin/yabiadmin/yabifeapp/static/javascript/YabiCommon.js
+++ yabiadmin/yabiadmin/yabifeapp/static/javascript/YabiCommon.js
@@ -105,52 +105,9 @@ Yabi.util.text = function(e, text) {
 };
 
 Yabi.util.doesGlobMatch = function(name, glob) {
-  var matcher = Yabi.util.Glob.create(glob);
-  return matcher.doesMatch(name);
+  var matcher = globToRegexp(glob, {extended: true});
+  return matcher.test(name);
 };
-
-Yabi.util.Glob = {};
-Yabi.util.Glob.create = function(glob) {
-  var that = {};
-  that.ESCAPE_REGEX = /[-[\]{}()*+?.,\\^$|#\s]/g;
-  that.regex = null;
-
-  that.getRegex = function() {
-    if (that.regex === null) {
-      that.regex = that.convertGlobToRegex(glob);
-    }
-    return that.regex;
-  };
-
-  that.doesMatch = function(name) {
-    var regex = that.convertGlobToRegex(glob);
-    return regex.test(name);
-  };
-
-  that.convertGlobToRegex = function(glob) {
-    var regex = '';
-    var c;
-    var i = 0;
-
-    while (i < glob.length) {
-      c = glob.charAt(i);
-      i += 1;
-      if (c === '*') {
-        regex += '.*';
-      } else {
-        regex += that.escapeCharacter(c);
-      }
-    }
-    return new RegExp(regex);
-  };
-
-  that.escapeCharacter = function(c) {
-    return c.replace(that.ESCAPE_REGEX, '\\$&');
-  };
-
-  return that;
-};
-
 
 Yabi.util.Status = {};
 Yabi.util.Status.getStatusImage = function(status) {
