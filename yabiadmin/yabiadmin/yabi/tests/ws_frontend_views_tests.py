cc192869c87356f130c96bb7d91e30ea57bcd993
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2014-07-23 12:11:36 +0800

    Test for ws/fs/ls without URI (top-level ls listing visible, non-dynamic BackendCredentials)

--- yabiadmin/yabiadmin/yabi/tests/ws_frontend_views_tests.py
+++ yabiadmin/yabiadmin/yabi/tests/ws_frontend_views_tests.py
@@ -4,18 +4,36 @@ from django.utils import unittest as unittest
 from django.test.client import Client
 from model_mommy import mommy
 from yabiadmin.yabi.ws_frontend_views import munge_name
-from yabiadmin.yabi.models import User, Backend, Tool, ToolSet, ToolGroup
+from yabiadmin.yabi.models import User, Backend, BackendCredential, Credential, Tool, ToolSet, ToolGroup
 from django.core.cache import cache
 
 from django.utils import simplejson as json
 
 
-class WsMenuTest(unittest.TestCase):
+USER = 'demo'
+ADMIN_USER = 'admin'
+
+
+class WSTestCase(unittest.TestCase):
+
     def setUp(self):
-        self.tool = None
+        unittest.TestCase.setUp(self)
         self.client = Client()
+
+    def login_fe(self, user, password=None):
+        if password is None:
+            password = user
+        response = self.client.post('/login', {
+            'username': user, 'password': password})
+        assert response.status_code == 302, "Couldn't log in to FE"
+
+
+class WsMenuTest(WSTestCase):
+    def setUp(self):
+        WSTestCase.setUp(self)
+        self.tool = None
         cache.clear()
-        self.login_fe('admin')
+        self.login_fe(ADMIN_USER)
 
     def tearDown(self):
         if self.tool:
@@ -55,9 +73,9 @@ class WsMenuTest(unittest.TestCase):
         self.tool = self.add_new_tool()
         cache.clear()
         admin_menu = self.client.get('/ws/menu')
-        self.login_fe('demo')
-        demo_menu = self.client.get('/ws/menu')
-        self.assertNotEqual(admin_menu.content, demo_menu.content,
+        self.login_fe(USER)
+        user_menu = self.client.get('/ws/menu')
+        self.assertNotEqual(admin_menu.content, user_menu.content,
                             "Shouldn't get the same response from cache")
 
     def add_new_tool(self):
@@ -66,23 +84,16 @@ class WsMenuTest(unittest.TestCase):
             name='new-tool', display_name='new_tool',
             backend=null_backend, fs_backend=null_backend)
         test_tset = ToolSet.objects.create(name='test')
-        admin = User.objects.get(name='admin')
+        admin = User.objects.get(name=ADMIN_USER)
         test_tset.users.add(admin)
         select_data = ToolGroup.objects.get(name='select data')
         select_data.toolgrouping_set.create(tool_set=test_tset, tool=tool)
         return tool
 
-    def login_fe(self, user, password=None):
-        if password is None:
-            password = user
-        response = self.client.post('/login', {
-            'username': user, 'password': password})
-        assert response.status_code == 302, "Couldn't log in to FE"
-
 
 class TestWorkflowNameMunging(unittest.TestCase):
     def setUp(self):
-        self.user = User.objects.get(name='demo')
+        self.user = User.objects.get(name=USER)
         self.workflow = mommy.make('Workflow', name='Unmunged yet', user=self.user)
         self.munged_workflows = [
             mommy.make('Workflow', name='Munged', user=self.user),
@@ -110,3 +121,53 @@ class TestWorkflowNameMunging(unittest.TestCase):
     def test_already_munged_called_with_munged_name(self):
         name = munge_name(self.user.workflow_set, 'Munged (1)')
         self.assertEquals('Munged (3)', name)
+
+
+class TestLsWithExtraBackendCredentials(WSTestCase):
+    def setUp(self):
+        WSTestCase.setUp(self)
+        # Preloaded by quickstart
+        self.PRELOADED_BC = BackendCredential.objects.get(pk=3)
+        self.setUpExtraBackendCredentials()
+        self.login_fe(USER)
+        self.maxDiff = None
+
+    def setUpExtraBackendCredentials(self):
+        # just use first preloaded Credential of USER
+        credential = Credential.objects.filter(user__name=USER)[0]
+
+        self.be = mommy.make('Backend', scheme='sftp', hostname='some.host.com', port=2222, path='/some/home/')
+        self.dynbe = mommy.make('Backend', dynamic_backend=True)
+
+        self.bc = mommy.make('BackendCredential', backend=self.be, credential=credential, homedir='someusername', visible=True)
+        # Visible set to False, shouldn't show up
+        self.invisible_bc = mommy.make('BackendCredential', backend=self.be, credential=credential, homedir='other_dir', visible=False)
+
+        # Associated with Dynamic Backend, shouldn't show up
+        self.dynbe_bc = mommy.make('BackendCredential', backend=self.dynbe, credential=credential, homedir='another_dir', visible=True)
+
+    def tearDown(self):
+        self.bc.delete()
+        self.invisible_bc.delete()
+        self.dynbe_bc.delete()
+
+        self.be.delete()
+        self.dynbe.delete()
+
+    def test_ls_with_no_uri_returns_visible_backend_credentials(self):
+        response = self.client.get('/ws/fs/ls')
+        listing = json.loads(response.content)
+
+        # BackendCredentials with Visible False should never show up
+        # BackendCredentials associated with Dynamic Backends should never show up
+        # The listing will have the preloaded BackendCredential and the only
+        # BackendCredential we created with visible True and associated with a
+        # non-dynamic backend
+        expected_homedir_uri = 'sftp://%s@some.host.com:2222/some/home/someusername' % USER
+        self.assertEquals(
+            {USER: {
+                'files': [],
+                'directories': [
+                    [self.PRELOADED_BC.homedir_uri, 0, ''],
+                    [expected_homedir_uri, 0, '']]}},
+            listing)
