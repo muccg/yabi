b68f058bbf18a31817795d9f2672aac68f492209
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-08-13 15:20:52 +0800

    puppet: release 8.0.0

--- puppet/prod-ccg.pp
+++ puppet/prod-ccg.pp
@@ -13,7 +13,7 @@ node default {
 
   $django_config = {
     deployment             => 'prod',
-    release                => '7.2.7-3',
+    release                => '8.0.0-1',
     dbdriver               => 'django.db.backends.postgresql_psycopg2',
     dbserver               => $globals::dbhost_postgresql_ccg_prod,
     dbhost                 => $globals::dbhost_postgresql_ccg_prod,
