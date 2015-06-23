f138702e37182b5a829a8b5d4ea893b338c82543
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-06-23 16:20:20 +0800

    clean after staging

--- develop.sh
+++ develop.sh
@@ -97,7 +97,7 @@ ci_staging() {
     ccg ${AWS_STAGING_INSTANCE} drun:'cd yabi && fig -f fig-staging.yml rm --force -v'
     ccg ${AWS_STAGING_INSTANCE} drun:'cd yabi && fig -f fig-staging.yml build --no-cache webstaging'
     ccg ${AWS_STAGING_INSTANCE} drun:'cd yabi && fig -f fig-staging.yml up -d'
-    ccg ${AWS_STAGING_INSTANCE} drun:'docker-untagged || true'
+    ccg ${AWS_STAGING_INSTANCE} drun:'docker-clean || true'
 }
 
 
