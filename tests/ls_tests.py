e7b6facaa1cf0c5dc33d9e3191361cff3230403b
Tamas Szabo <szabtam@gmail.com>
2015-06-23 10:10:25 +0300

    Ignore dir size in ls tests.

--- tests/ls_tests.py
+++ tests/ls_tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from yabi.backend.utils import ls
+from copy import copy
 import os
 import shutil
 
@@ -78,14 +79,28 @@ class LsTestCase(TestCase):
 
         self.assertTrue(len(directories) == 2, "Expected 2 directories. listing = %s" % listing)
 
-        self.assertTrue(('foo', 4096, self.todays_date(), False) in directories, 'Subdirectories wrong foo: directories = %s listing = %s' % (directories, listing))
-        self.assertTrue(('bar', 4096, self.todays_date(), False) in directories, 'Subdirectories wrong bar: directories = %s listing = %s' % (directories, listing))
+        directories_without_size = map(ignore_size, directories)
+        self.assertTrue(('foo', self.todays_date(), False) in directories_without_size, 'Subdirectories wrong foo: directories = %s listing = %s' % (directories, listing))
+        self.assertTrue(('bar', self.todays_date(), False) in directories_without_size, 'Subdirectories wrong bar: directories = %s listing = %s' % (directories, listing))
 
         listing = ls(os.path.join(self.root, 'foo'))
+        foo_dir_key = "%sfoo/" % self.root
 
-        expected = {"%sfoo/" % self.root: {"files": [('a.txt', 1, self.todays_date(), False), ('b.txt', 1, self.todays_date(),  False), ("l.txt", 15, self.todays_date(),  True)], 'directories': [('baz', 4096, self.todays_date(), False )]}}
+        self.assertEquals(1, len(listing))
+        self.assertTrue(foo_dir_key in listing)
 
-        self.assertEquals(listing, expected, "subdirectory listing incorect: Expected: %s Actual: %s" % (expected, listing))
+        foo_listing = listing[foo_dir_key]
+        self.assertEquals(2, len(foo_listing))
+        self.assertTrue('files' in foo_listing)
+        self.assertTrue('directories' in foo_listing)
+
+        expected_files = [('a.txt', 1, self.todays_date(), False), ('b.txt', 1, self.todays_date(),  False), ("l.txt", 15, self.todays_date(),  True)]
+        self.assertEquals(foo_listing['files'], expected_files)
+
+        expected_dirs = [('baz', self.todays_date(), False)]
+        foo_dirs = foo_listing['directories']
+        foo_dirs_without_size = map(ignore_size, foo_dirs)
+        self.assertEquals(expected_dirs, foo_dirs_without_size)
 
     def test_recursive_ls(self):
         import json
@@ -93,13 +108,22 @@ class LsTestCase(TestCase):
         # This is what was produced by old ls parsing code
         
         self.old_listing = {"/tmp/lstestdir/foo/baz/": {"files": [], "directories": []},
-                            "/tmp/lstestdir/": {"files": [("d.txt", 1, self.todays_date(), False)], "directories": [("bar", 4096, self.todays_date(), False), ("foo", 4096, self.todays_date(), False)]},
-                            "/tmp/lstestdir/foo/": {"files": [("a.txt", 1, self.todays_date(), False), ("b.txt", 1, self.todays_date(), False), ("l.txt", 15, self.todays_date(), True)], "directories": [("baz", 4096, self.todays_date(), False)]},
+                            "/tmp/lstestdir/": {"files": [("d.txt", 1, self.todays_date(), False)], "directories": [("bar", 1096, self.todays_date(), False), ("foo", 1096, self.todays_date(), False)]},
+                            "/tmp/lstestdir/foo/": {"files": [("a.txt", 1, self.todays_date(), False), ("b.txt", 1, self.todays_date(), False), ("l.txt", 15, self.todays_date(), True)], "directories": [("baz", 1096, self.todays_date(), False)]},
                             "/tmp/lstestdir/bar/": {"files": [("c.txt", 1, self.todays_date(), False)], "directories": []},
                             }
+
+        def remove_dir_sizes(listing):
+            new_listing = {}
+            for k, v in listing.items():
+                new_v = copy(v)
+                new_v['directories'] = map(ignore_size, v['directories'])
+                new_listing[k] = new_v
+            return new_listing
+
         listing = ls(self.root, recurse=True)
 
-        self.assertEquals(self.old_listing, listing, "old listing = %s\n\n\nnew listing=%s" % (self.old_listing, listing))
+        self.assertEquals(remove_dir_sizes(self.old_listing), remove_dir_sizes(listing), "old listing = %s\n\n\nnew listing=%s" % (self.old_listing, listing))
 
     def test_ls_on_file(self):
         # take the file /tmp/lstestdir/foo/b.txt
@@ -114,3 +138,8 @@ class LsTestCase(TestCase):
     def tearDown(self):
         self.remove_folder_structure()
         TestCase.tearDown(self)
+
+
+def ignore_size(directory):
+    name, size, date, is_symlink = directory
+    return (name, date, is_symlink)
