6500178e753d18d5dc2d3a53290505f43f34c01c
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2015-09-15 10:18:16 +0300

    This proxy view isn't used anywhere.

--- yabi/yabi/yabi/views.py
+++ yabi/yabi/yabi/views.py
@@ -14,57 +14,12 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import httplib
-from urllib import urlencode
-import copy
 import os
 from django.conf.urls import *
 from django.conf import settings
 from django.http import HttpResponse
 import logging
 logger = logging.getLogger(__name__)
-
-
-# proxy view to pass through all requests set up in urls.py
-def proxy(request, url, server, base):
-    logger.debug(url)
-    logger.debug(server)
-    logger.debug(base)
-
-    # TODO CODEREVIEW
-    # Is is possible to post to a page and still send get params,
-    # are they dropped by this proxy. Would it be possible to override yabiusername by
-    # crafting a post and sending yabiusername as a get param as well
-
-    if request.method == "GET":
-        # resource = "%s?%s" % (os.path.join(base, url), request.META['QUERY_STRING']+"&yabiusername=%s"%quote(request.user.username) )
-        resource = "%s?%s" % (os.path.join(base, url), request.META['QUERY_STRING'])
-        logger.debug('Proxying get: %s%s' % (server, resource))
-        conn = httplib.HTTPConnection(server)
-        conn.request(request.method, resource)
-        r = conn.getresponse()
-
-    elif request.method == "POST":
-        resource = os.path.join(base, url)
-        post_params = copy.copy(request.POST)
-        # post_params['yabiusername'] = request.user.username
-        logger.debug('Proxying post: %s%s' % (server, resource))
-        data = urlencode(post_params)
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        conn = httplib.HTTPConnection(server)
-        conn.request(request.method, resource, data, headers)
-        r = conn.getresponse()
-
-    data = r.read()
-    response = HttpResponse(data, status=int(r.status))
-
-    if r.getheader('content-disposition', None):
-        response['content-disposition'] = r.getheader('content-disposition')
-
-    if r.getheader('content-type', None):
-        response['content-type'] = r.getheader('content-type')
-
-    return response
 
 
 def status_page(request):
