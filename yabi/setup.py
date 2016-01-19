cbaaf721b63cfac64a7ae6e6f960c28c6785fdef
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-01-19 18:53:48 +0200

    Version 9.8.3 -> 9.8.4

--- yabi/setup.py
+++ yabi/setup.py
@@ -25,7 +25,7 @@ if not importlib_available:
     install_requires.append('importlib>=1.0.1,<1.1.0')
 
 setup(name='yabi',
-    version='9.8.3',
+    version='9.8.4',
     description='Yabi',
     long_description='Yabi web interface',
     author='Centre for Comparative Genomics',
