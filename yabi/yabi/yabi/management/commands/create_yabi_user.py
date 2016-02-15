22b5e6ff8f9e2e9a2c8e5445ed3a9bd0a636355d
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-02-15 19:07:11 +0200

    YABI-601: Add custom command to add users from the command line

---
+++ yabi/yabi/yabi/management/commands/create_yabi_user.py
@@ -0,0 +1,97 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User as DjangoUser
+from django.db import transaction
+from mako.template import Template
+from yabi.yabi.models import BackendCredential, Credential, User, ToolSet
+
+
+class Command(BaseCommand):
+    help = 'Creates a yabi user'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', help='The username of the new user')
+        parser.add_argument('--email', help='The email address of the new user')
+        parser.add_argument('--password', help='The password of the new user')
+        parser.add_argument('--first-name', help='The first name of the new user')
+        parser.add_argument('--last-name', help='The last name of the new user')
+        parser.add_argument('--is-staff', default=None, action='store_true', help='Set if the new user should be a staff member')
+        parser.add_argument('--is-superuser', default=None, action='store_true', help='Set if the new user should be a superuser')
+        parser.add_argument('--replicate-user', help="The username of an existing user who's tools and backnd credentials this user should also have")
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        username = options['username']
+        if DjangoUser.objects.filter(username=username).exists():
+            raise CommandError("Couldn't create user '%s'!\nUser with same username already exists." % username)
+        skeleton_user = None
+        if options['replicate_user']:
+            try:
+                skeleton_user = User.objects.get(name=options['replicate_user'])
+            except User.DoesNotExist:
+                raise CommandError("User '%s' doesn't exist" % options['replicate_user'])
+
+        self.stdout.write("Creating user '%s'." % username)
+        extra_arguments = {}
+        for extra_arg in ('email', 'password', 'first_name', 'last_name'):
+            if options[extra_arg]:
+                extra_arguments[extra_arg] = options[extra_arg]
+
+        try:
+            new_user = DjangoUser.objects.create_user(username, **extra_arguments)
+            handle_is_staff_and_is_superuser(new_user, options, skeleton_user)
+            if skeleton_user is not None:
+                replicate_other_user(skeleton_user, new_user.user)
+
+        except Exception, e:
+            raise CommandError("Couldn't create user '%s'!\n%s" % (username, e))
+
+        self.stdout.write("Successfully created user '%s'." % username)
+
+
+def handle_is_staff_and_is_superuser(new_user, options, skeleton_user):
+    print options
+    is_staff = options['is_staff']
+    if is_staff is None and skeleton_user is not None:
+        is_staff = skeleton_user.user.is_staff
+    is_superuser = options['is_superuser']
+    if is_superuser is None and skeleton_user is not None:
+        is_superuser = skeleton_user.user.is_superuser
+
+    if is_staff is not None:
+        new_user.is_staff = is_staff
+    if is_superuser is not None:
+        new_user.is_superuser = is_superuser
+    if not (is_staff is None and is_superuser is None):
+        new_user.save()
+
+
+def replicate_other_user(source_user, user):
+    """Replicates toolsets and backends of the source_user into user.
+
+    Passwords and keys of credentials aren't copied (they are encrypted anyways).
+    Most string fields can use the ${username} Mako variable to customise their value."""
+
+    for toolset in ToolSet.objects.filter(users=source_user):
+        toolset.users.add(user)
+        toolset.save()
+
+    def render(str):
+        return Template(str).render(username=user.name)
+
+    credentials = {}
+    for cred in source_user.credential_set.all():
+        new_user_cred = Credential.objects.create(
+            description=render(cred.description),
+            username=render(cred.username),
+            user=user,
+            expires_on=cred.expires_on)
+        credentials[cred.pk] = new_user_cred
+
+    for bc in BackendCredential.objects.filter(credential__user=source_user):
+        BackendCredential.objects.create(
+            backend=bc.backend,
+            credential=credentials[bc.credential.pk],
+            homedir=render(bc.homedir),
+            visible=bc.visible,
+            default_stageout=bc.default_stageout,
+            submission=render(bc.submission))
