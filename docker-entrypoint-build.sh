fccd23cd337b6360157bd02394b114e40d9a91d2
aahunter <ahunter@ccg.murdoch.edu.au>
2016-09-16 16:17:54 +0800

    correct prod build

--- docker-entrypoint-build.sh
+++ docker-entrypoint-build.sh
@@ -16,11 +16,14 @@ if [ "$1" = 'releasetarball' ]; then
     git ls-remote ${PROJECT_SOURCE} ${GIT_TAG} > .version
 
     # Note: Environment vars are used to control the behaviour of pip (use local devpi for instance)
+    pip install ${PIP_OPTS} --upgrade -r yabi/runtime-requirements.txt
     pip install -e ${PROJECT_NAME}
+    pip install ${PIP_OPTS} --upgrade -r yabish/requirements.txt
+    pip install -e yabish
     set +x
 
     # create release tarball
-    DEPS="/env /app/uwsgi /app/docker-entrypoint.sh /app/${PROJECT_NAME}"
+    DEPS="/env /app/uwsgi /app/docker-entrypoint.sh /app/${PROJECT_NAME} /app/yabish"
     cd /data
     exec tar -cpzf ${PROJECT_NAME}-${GIT_TAG}.tar.gz ${DEPS}
 fi
