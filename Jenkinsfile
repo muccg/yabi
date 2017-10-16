2b08b02b34fee5fcc0f5bc2436a246057b4e47f9
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2017-10-16 14:58:32 +0800

    docker build refresh

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,47 +1,108 @@
 #!groovy
 
 node {
-    env.DOCKER_USE_HUB = 1
     def deployable_branches = ["master", "next_release"]
+    def testResults = []
+    def artifacts = []
 
     stage('Checkout') {
         checkout scm
     }
 
-    stage('Docker dev build') {
+    dockerStage('Dev build') {
         echo "Branch is: ${env.BRANCH_NAME}"
         echo "Build is: ${env.BUILD_NUMBER}"
-        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-            sh './develop.sh docker_warm_cache'
-            sh './develop.sh dev_build'
-            sh './develop.sh check_migrations'
-        }
+        sh('''
+            ./develop.sh build base
+            ./develop.sh build builder
+            ./develop.sh build dev
+            ./develop.sh check-migrations
+        ''')
     }
 
-    stage('Unit tests') {
-        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-            sh './develop.sh runtests'
-        }
-        step([$class: 'JUnitResultArchiver', testResults: '**/data/tests/*.xml'])
+    testResults = ['**/data/tests/*.xml']
+    dockerStage('Unit tests', [], testResults) {
+        sh('./develop.sh unittests teststack')
+    }
+
+    artifacts = ['**/data/selenium/dev/scratch/*.png',
+                 '**/data/selenium/dev/log/*.log',
+                 'lettuce-teststack.log',
+                 'lettuce-selenium.log']
+    testResults = ['**/data/selenium/dev/scratch/*.xml']
+    dockerStage('Dev lettuce tests', artifacts, testResults) {
+        sh('''
+            ./develop.sh lettuce teststack
+        ''')
     }
 
     if (deployable_branches.contains(env.BRANCH_NAME)) {
 
-        stage('Docker prod build') {
-            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-                sh './develop.sh prod_build'
-            }
+        dockerStage('Prod build') {
+            sh('''
+                ./develop.sh run-builder
+                ./develop.sh build prod
+            ''')
         }
 
-        stage('Publish docker image') {
+        artifacts = ['**/data/selenium/prod/scratch/*.png',
+                     '**/data/selenium/prod/log/*.log',
+                     'lettuce-prod.log',
+                     'lettuce-selenium.log']
+        testResults = ['**/data/selenium/prod/scratch/*.xml']
+        dockerStage('Prod lettuce tests', artifacts, testResults) {
+            sh('''
+                ./develop.sh lettuce prod
+            ''')
+        }
+
+        dockerStage('Publish docker image') {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerbot',
                               usernameVariable: 'DOCKER_USERNAME',
                               passwordVariable: 'DOCKER_PASSWORD']]) {
-                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-                    sh './develop.sh ci_docker_login'
-                    sh './develop.sh publish_docker_image'
-                }
+                sh("""
+                    docker login -u "${env.DOCKER_USERNAME}" --password="${env.DOCKER_PASSWORD}"
+                    ./develop.sh push prod
+                """)
             }
         }
     }
+}
+
+
+/*
+ * dockerStage
+ *
+ * Custom stage that wraps the stage in timestamps and AnsiColorBuildWrapper
+ * Prior to exit wrfy is used to kill all running containers and cleanup.
+ */
+def dockerStage(String label,
+                List<String> artifacts=[],
+                List<String> testResults=[],
+                Closure body) {
+
+    stage(label) {
+        try {
+            timestamps {
+                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName':    'XTerm']) {
+                    body.call()
+                }
+            }
+        } catch (Exception e) {
+            currentBuild.result = 'FAILURE'
+            throw e
+        } finally {
+            for (artifact in artifacts) {
+                step([$class: 'ArtifactArchiver', artifacts: artifact, fingerprint: false, excludes: null])
+            }
+            for (testResult in testResults) {
+                step([$class: 'JUnitResultArchiver', testResults: testResult])
+            }
+            sh('''
+                /env/bin/wrfy kill-all --force
+                /env/bin/wrfy scrub --force
+            ''')
+        }
+    }
+
 }
--- develop.sh
+++ develop.sh
@@ -1,164 +1,48 @@
 #!/bin/sh
-#
-# Script for dev, test and ci
-#
-
-: ${PROJECT_NAME:='yabi'}
-. ./lib.sh
-
-# break on error
+set +x
 set -e
 
-ACTION="$1"
+: "${CCG_DOCKER_ORG:=muccg}"
+: "${CCG_COMPOSER:=ccg-composer}"
+: "${CCG_COMPOSER_VERSION:=latest}"
+: "${CCG_PIP_PROXY=0}"
+: "${CCG_HTTP_PROXY=0}"
 
-# build a docker image and start stack on staging using docker-compose
-ci_docker_staging() {
-    info 'ci docker staging'
-    ssh ubuntu@staging.ccgapps.com.au << EOF
-      mkdir -p ${PROJECT_NAME}/data
-      chmod o+w ${PROJECT_NAME}/data
-EOF
+export CCG_DOCKER_ORG CCG_COMPOSER CCG_COMPOSER_VERSION CCG_PIP_PROXY CCG_HTTP_PROXY
 
-    scp docker-compose-*.yml ubuntu@staging.ccgapps.com.au:${PROJECT_NAME}/
-
-    # TODO This doesn't actually do a whole lot, some tests should be run against the staging stack
-    ssh ubuntu@staging.ccgapps.com.au << EOF
-      cd ${PROJECT_NAME}
-      docker-compose -f docker-compose-staging.yml stop
-      docker-compose -f docker-compose-staging.yml kill
-      docker-compose -f docker-compose-staging.yml rm --force -v
-      docker-compose -f docker-compose-staging.yml up -d
-EOF
-}
-
-
-docker_staging_lettuce() {
-    _selenium_stack_up
-
-    set -x
+# ensure we have an .env file
+ENV_FILE_OPT=''
+if [ -f .env ]; then
+    ENV_FILE_OPT='--env-file .env'
     set +e
-    ( docker-compose --project-name ${PROJECT_NAME} -f docker-compose-staging-lettuce.yml rm --force || exit 0 )
-    (${CMD_ENV}; docker-compose --project-name ${PROJECT_NAME} -f docker-compose-staging-lettuce.yml build)
-    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-staging-lettuce.yml up
-    rval=$?
+    . ./.env > /dev/null 2>&1
     set -e
-    set +x
+else
+    echo ".env file not found, settings such as project name and proxies will not be set"
+fi
 
-    _selenium_stack_down
+#  Pass through the ip of the host if we can
+# There is no docker0 interface on Mac OS, so don't do any proxy detection
+if [ "$(uname)" != "Darwin" ]; then
+    set +e
+    DOCKER_ROUTE=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+    set -e
+    export DOCKER_ROUTE
+fi
 
-    exit $rval
-}
+TTY_OPTS=
+if [ -t 0 ]; then
+    TTY_OPTS='--interactive --tty'
+fi
 
-
-# lint using flake8
-python_lint() {
-    info "python lint"
-    docker-compose -f docker-compose-build.yml run --rm lint flake8 yabi/yabi yabish/yabishell --exclude=migrations --ignore=E501 --count
-    success "python lint"
-}
-
-
-# lint js, assumes closure compiler
-js_lint() {
-    info "js lint"
-    JSFILES="yabi/yabi/yabifeapp/static/javascript/*.js yabi/yabi/yabifeapp/static/javascript/account/*.js"
-    for JS in $JSFILES
-    do
-        docker-compose -f docker-compose-build.yml run lint gjslint --disable 0131 --max_line_length 100 --nojsdoc $JS
-    done
-    success "js lint"
-}
-
-
-echo ''
-info "$0 $@"
-docker_options
-git_tag
-
-case $ACTION in
-pythonlint)
-    python_lint
-    ;;
-jslint)
-    js_lint
-    ;;
-dev)
-    start_dev
-    ;;
-dev_build)
-    create_base_image
-    create_build_image
-    create_dev_image
-    ;;
-django-admin)
-    shift
-    django_admin $@
-    ;;
-check_migrations)
-    check_migrations
-    ;;
-releasetarball)
-    create_release_tarball
-    ;;
-prod)
-    start_prod
-    ;;
-prod_build)
-    create_base_image
-    create_build_image
-    create_release_tarball
-    create_prod_image
-    ;;
-baseimage)
-    create_base_image
-    ;;
-buildimage)
-    create_build_image
-    ;;
-prodimage)
-    create_prod_image
-    ;;
-devimage)
-    create_dev_image
-    ;;
-publish_docker_image)
-    publish_docker_image
-    ;;
-runtests)
-    create_base_image
-    create_build_image
-    create_dev_image
-    run_unit_tests
-    ;;
-start_test_stack)
-    start_test_stack
-    ;;
-start_seleniumhub)
-    start_seleniumhub
-    ;;
-docker_warm_cache)
-    docker_warm_cache
-    ;;
-ci_docker_login)
-    ci_docker_login
-    ;;
-ci_docker_staging)
-    _ci_ssh_agent
-    ci_docker_staging
-    ;;
-docker_staging_lettuce)
-    docker_staging_lettuce
-    ;;
-dev_lettuce)
-    dev_lettuce
-    ;;
-lettuce)
-    dev_lettuce
-    ;;
-prod_lettuce)
-    prod_lettuce
-    ;;
-*)
-    usage
-    ;;
-esac
+ENV_OPTS="$(env | sort | cut -d= -f1 | grep "^CCG_[a-zA-Z0-9_]*$" | awk '{print "-e", $1}')"
+# shellcheck disable=SC2086 disable=SC2048
+docker run --rm ${TTY_OPTS} ${ENV_FILE_OPT} \
+    ${ENV_OPTS} \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock  \
+    -v "$(pwd)":"$(pwd)" \
+    -v "${HOME}"/.docker:/data/.docker \
+    -w "$(pwd)" \
+    "${CCG_DOCKER_ORG}"/"${CCG_COMPOSER}":"${CCG_COMPOSER_VERSION}" \
+    "$@"
