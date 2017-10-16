82ef372540a6db582db188fef8bdd38417abe99a
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2017-10-16 12:06:30 +0800

    bump 9.9.6

--- yabi/setup.py
+++ yabi/setup.py
@@ -25,7 +25,7 @@ if not importlib_available:
     install_requires.append('importlib>=1.0.1,<1.1.0')
 
 setup(name='yabi',
-    version='9.9.5',
+    version='9.9.6',
     description='Yabi',
     long_description='Yabi web interface',
     author='Centre for Comparative Genomics',
